6a6d3f035a3dc0810481909eb0dc47b908582151
DingDongDeng <tpea999@gmail.com>
2021-12-30 00:15:12 +0900

    init commit

---
+++ src/test/java/com/dingdongdeng/coinautotrading/CoinautotradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingdongdeng.coinautotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinautotradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
