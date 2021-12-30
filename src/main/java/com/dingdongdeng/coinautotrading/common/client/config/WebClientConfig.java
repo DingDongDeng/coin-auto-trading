6992e7869e0018400f5587cac45ffec243c53eee
DingDongDeng <etea583@naver.com>
2021-12-30 16:51:02 +0900

    WebClientConfig 리팩토링

--- src/main/java/com/dingdongdeng/coinautotrading/common/client/config/WebClientConfig.java
+++ src/main/java/com/dingdongdeng/coinautotrading/common/client/config/WebClientConfig.java
@@ -1,34 +1,19 @@
-package com.dingdongdeng.coinautotrading.upbit.client.config;
+package com.dingdongdeng.coinautotrading.common.client.config;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import java.time.Duration;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
-@RequiredArgsConstructor
-@Configuration
-public class WebclientConfig {
+public abstract class WebClientConfig {
 
-    private final ClientResourceProperties properties;
-
-    @Bean
-    public WebClient upbitWebClient() {
-        return makeWebClient(properties.getBaseUrl(), properties.getReadTimeout(), properties.getConnectionTimeout());
-    }
-
-    private WebClient makeWebClient(String baseUrl, int readTimeout, int connectionTimeout) {
+    protected WebClient makeWebClient(String baseUrl, int readTimeout, int connectionTimeout) {
         HttpClient httpClient = HttpClient.create()
             .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionTimeout)
             .responseTimeout(Duration.ofMillis(5000))
@@ -41,22 +26,8 @@ public class WebclientConfig {
             .baseUrl(baseUrl)
             .defaultHeaders(httpHeaders -> {
                 httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-                httpHeaders.set("Authorization", makeAuthenticationToken());
             })
             .clientConnector(new ReactorClientHttpConnector(httpClient))
             .build();
-    }
-
-    private String makeAuthenticationToken() {
-        String accessKey = properties.getAccessKey();
-        String secretKey = properties.getSecretKey();
-
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        String jwtToken = JWT.create()
-            .withClaim("access_key", accessKey)
-            .withClaim("nonce", UUID.randomUUID().toString())
-            .sign(algorithm);
-
-        return "Bearer " + jwtToken;
     }
 }
---
+++ src/main/java/com/dingdongdeng/coinautotrading/upbit/client/config/UpbitWebClientConfig.java
@@ -0,0 +1,20 @@
+package com.dingdongdeng.coinautotrading.upbit.client.config;
+
+import com.dingdongdeng.coinautotrading.common.client.config.WebClientConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+@Configuration
+public class UpbitWebClientConfig extends WebClientConfig {
+
+    private final UpbitClientResourceProperties properties;
+
+    @Bean
+    public WebClient upbitWebClient() {
+        return makeWebClient(properties.getBaseUrl(), properties.getReadTimeout(), properties.getConnectionTimeout());
+    }
+
+}
