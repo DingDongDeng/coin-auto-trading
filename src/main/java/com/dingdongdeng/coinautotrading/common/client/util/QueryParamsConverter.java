9ad0ac91603ae35076ac2f4b55c80f2b2471eb3e
DingDongDeng <etea583@naver.com>
2021-12-30 22:15:31 +0900

    QueryParamsConverter 생성

---
+++ src/main/java/com/dingdongdeng/coinautotrading/common/client/util/QueryParamsConverter.java
@@ -0,0 +1,32 @@
+package com.dingdongdeng.coinautotrading.common.client.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@RequiredArgsConstructor
+@Component
+public class QueryParamsConverter {
+
+    private final ObjectMapper objectMapper;
+
+    public MultiValueMap<String, String> convert(Object object) {
+        try {
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            if (Objects.isNull(object)) {
+                return params;
+            }
+            Map<String, String> map = objectMapper.convertValue(object, new TypeReference<Map<String, String>>() {
+            });
+            params.setAll(map);
+            return params;
+        } catch (Exception e) {
+            throw new IllegalStateException("fail generate query params", e);
+        }
+    }
+}
