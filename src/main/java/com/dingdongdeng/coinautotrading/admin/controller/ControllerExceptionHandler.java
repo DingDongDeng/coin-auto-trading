e679aea76f97cb638f113e5851cd10dc8a7adde1
DingDongDeng <etea583@naver.com>
2021-12-31 03:02:37 +0900

    ControllerExceptionHandler 오타 수정

--- src/main/java/com/dingdongdeng/coinautotrading/admin/controller/ControllerExceptionHandler.java
+++ src/main/java/com/dingdongdeng/coinautotrading/admin/controller/ControllerExceptionHandler.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice(basePackages = "com.dingdongdeng.coinautotrading.autotrading.controller")
+@RestControllerAdvice(basePackages = "com.dingdongdeng.coinautotrading.admin.controller")
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public String aa(Exception e) {
+    public String exceptionHandler(Exception e) {
         log.error(e.getMessage(), e);
         return "occured exception : " + e.getMessage();
     }
