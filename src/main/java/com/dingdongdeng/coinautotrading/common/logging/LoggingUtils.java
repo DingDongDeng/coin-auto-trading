5e8fff73bbccceab789d6fb439fdf1b9d6bb5ec8
DingDongDeng <etea583@naver.com>
2022-01-02 01:11:05 +0900

    mdc 로깅을 위한 LoggingUtils 생성 및 logback 포멧 수정

---
+++ src/main/java/com/dingdongdeng/coinautotrading/common/logging/LoggingUtils.java
@@ -0,0 +1,30 @@
+package com.dingdongdeng.coinautotrading.common.logging;
+
+import java.util.Map;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.slf4j.MDC;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class LoggingUtils {
+
+    private static void put(String key, String value) {
+        MDC.put(key, value);
+    }
+
+    public static Map<String, String> getLogData() {
+        return MDC.getCopyOfContextMap();
+    }
+
+    public static void setLogData(Map<String, String> contextMap) {
+        if (Objects.nonNull(contextMap)) {
+            MDC.setContextMap(contextMap);
+        }
+    }
+
+    public static void clear() {
+        MDC.clear();
+    }
+
+}
