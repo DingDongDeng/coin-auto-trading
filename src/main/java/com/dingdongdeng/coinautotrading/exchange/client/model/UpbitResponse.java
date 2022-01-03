55f2d7f3ff7c5c072ab8fabf480fad18c21771c3
DingDongDeng <etea583@naver.com>
2022-01-03 19:33:21 +0900

    업비트 주문 취소 api 응답값 데이터타입 수정

--- src/main/java/com/dingdongdeng/coinautotrading/exchange/client/model/UpbitResponse.java
+++ src/main/java/com/dingdongdeng/coinautotrading/exchange/client/model/UpbitResponse.java
@@ -210,7 +210,7 @@ public class UpbitResponse {
         @JsonProperty("ord_type")
         private String ordType; //	주문 방식	String
         @JsonProperty("price")
-        private String price; //	주문 당시 화폐 가격	NumberString
+        private Double price; //	주문 당시 화폐 가격	NumberString
         @JsonProperty("state")
         private String state; //	주문 상태	String
         @JsonProperty("market")
@@ -218,21 +218,21 @@ public class UpbitResponse {
         @JsonProperty("created_at")
         private String createdAt; //	주문 생성 시간	String
         @JsonProperty("volume")
-        private String volume; //	사용자가 입력한 주문 양	NumberString
+        private Double volume; //	사용자가 입력한 주문 양	NumberString
         @JsonProperty("remaining_volume")
-        private String remainingVolume; //	체결 후 남은 주문 양	NumberString
+        private Double remainingVolume; //	체결 후 남은 주문 양	NumberString
         @JsonProperty("reserved_fee")
-        private String reservedFee; //	수수료로 예약된 비용	NumberString
+        private Double reservedFee; //	수수료로 예약된 비용	NumberString
         @JsonProperty("remaining_fee")
-        private String remainingFee; //	남은 수수료	NumberString
+        private Double remainingFee; //	남은 수수료	NumberString
         @JsonProperty("paid_fee")
-        private String paidFee; //	사용된 수수료	NumberString
+        private Double paidFee; //	사용된 수수료	NumberString
         @JsonProperty("locked")
-        private String locked; //	거래에 사용중인 비용	NumberString
+        private Double locked; //	거래에 사용중인 비용	NumberString
         @JsonProperty("executed_volume")
-        private String executedVolume; //	체결된 양	NumberString
+        private Double executedVolume; //	체결된 양	NumberString
         @JsonProperty("trade_count")
-        private String tradeCount; //	해당 주문에 걸린 체결 수	Integer
+        private Integer tradeCount; //	해당 주문에 걸린 체결 수	Integer
     }
 
 }
