7e4b87b1366f6cd99734beea69ced34ce2dd48c7
DingDongDeng <etea583@naver.com>
2022-01-03 23:27:36 +0900

    ProcessAccountParam 불필요 필드 제거

--- src/main/java/com/dingdongdeng/coinautotrading/exchange/processor/model/ProcessAccountParam.java
+++ src/main/java/com/dingdongdeng/coinautotrading/exchange/processor/model/ProcessAccountParam.java
@@ -9,10 +9,5 @@ import lombok.ToString;
 @Builder
 public class ProcessAccountParam {
 
-    private String currency; // 화폐를 의미하는 영문 대문자 코드
-    private Double balance; // 주문가능 금액/수량
-    private Double locked; // 주문 중 묶여있는 금액/수량
-    private Double avgBuyPrice; // 매수평균가
-    private boolean avgBuyPriceModified; // 매수평균가 수정 여부
-    private String unitCurrency; // 평단가 기준 화폐
+
 }
