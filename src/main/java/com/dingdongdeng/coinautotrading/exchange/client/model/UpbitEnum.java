d86f53d64f57c84451a1ffbd4ca9687432356762
DingDongDeng <etea583@naver.com>
2022-01-03 21:25:32 +0900

    UpbitEnum 수정

--- src/main/java/com/dingdongdeng/coinautotrading/exchange/client/model/UpbitEnum.java
+++ src/main/java/com/dingdongdeng/coinautotrading/exchange/client/model/UpbitEnum.java
@@ -2,6 +2,7 @@ package com.dingdongdeng.coinautotrading.exchange.client.model;
 
 import com.dingdongdeng.coinautotrading.common.type.OrderType;
 import com.dingdongdeng.coinautotrading.common.type.PriceType;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,45 +12,36 @@ public class UpbitEnum {
     @Getter
     @AllArgsConstructor
     public enum OrdType {
-        limit("지정가 주문"),
-        price("시장가 주문(매수)"),
-        market("시장가 주문(매도)"),
+        limit("지정가 주문", PriceType.LIMIT_PRICE),
+        price("시장가 주문(매수)", PriceType.BUYING_MARKET_PRICE),
+        market("시장가 주문(매도)", PriceType.SELLING_MARKET_PRICE),
         ;
         private String desc;
+        private PriceType priceType;
 
         public static OrdType of(PriceType priceType) {
-            if (priceType == PriceType.LIMIT_PRICE) {
-                return limit;
-            }
-            if (priceType == PriceType.BUYING_MARKET_PRICE) {
-                return price;
-            }
-
-            if (priceType == PriceType.SELLING_MARKET_PRICE) {
-                return market;
-            }
-            throw new NoSuchElementException(priceType.name());
+            return Arrays.stream(OrdType.values())
+                .filter(type -> type.getPriceType() == priceType)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException(priceType.name()));
         }
     }
 
     @Getter
     @AllArgsConstructor
     public enum Side {
-        bid("매수"),
-        ask("매도"),
+        bid("매수", OrderType.BUYING),
+        ask("매도", OrderType.SELLING),
         ;
 
         private String desc;
+        private OrderType orderType;
 
         public static Side of(OrderType orderType) {
-            if (orderType == OrderType.BUYING) {
-                return bid;
-            }
-
-            if (orderType == OrderType.SELLING) {
-                return ask;
-            }
-            throw new NoSuchElementException(orderType.name());
+            return Arrays.stream(Side.values())
+                .filter(type -> type.getOrderType() == orderType)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException(orderType.name()));
         }
     }
 }
