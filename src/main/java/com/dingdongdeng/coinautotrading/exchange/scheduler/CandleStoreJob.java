084cdacbd997bda202c58b6e27d4780791fcaf65
DingDongDeng <etea583@naver.com>
2022-01-16 16:55:31 +0900

    CandleStoreJob 기본적인 로직만 작성

--- src/main/java/com/dingdongdeng/coinautotrading/exchange/scheduler/CandleStoreJob.java
+++ src/main/java/com/dingdongdeng/coinautotrading/exchange/scheduler/CandleStoreJob.java
@@ -1,6 +1,9 @@
 package com.dingdongdeng.coinautotrading.exchange.scheduler;
 
+import com.dingdongdeng.coinautotrading.common.type.CoinType;
 import com.dingdongdeng.coinautotrading.exchange.service.ExchangeCandleService;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
@@ -11,11 +14,14 @@ import org.quartz.JobExecutionException;
 @Slf4j
 public abstract class CandleStoreJob implements Job {
 
+    private final List<CoinType> TARGET_COINT_LIST = List.of(CoinType.ETHEREUM);
     private final ExchangeCandleService exchangeCandleService;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         log.info("run CandleStorJob");
+        exchangeCandleService.store(TARGET_COINT_LIST, null, LocalDateTime.now());
+
         //fixme CompletedFuture를 활용할수 있는 구조로 해보자
         // transaction 단위는 coinType단위로 끝내서 너무 오랫동안 트랜잭션 잡고있지 않게하자
         // 중복 저장의 위험은 unique 컬럼을 설정해서 방지하는게 좋을거같아(거의 발생안할 이슈라고 판단됨)
--- src/main/java/com/dingdongdeng/coinautotrading/exchange/service/ExchangeCandleService.java
+++ src/main/java/com/dingdongdeng/coinautotrading/exchange/service/ExchangeCandleService.java
@@ -1,5 +1,10 @@
 package com.dingdongdeng.coinautotrading.exchange.service;
 
+import com.dingdongdeng.coinautotrading.common.type.CoinType;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ExchangeCandleService {
 
+    void store(List<CoinType> coinTypeList, LocalDateTime start, LocalDateTime end);
 }
