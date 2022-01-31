84cec61763b2c2a1ccbf7587da97262b28248d27
DingDongDeng <etea583@naver.com>
2022-02-01 02:18:00 +0900

    IndexCalculator에 rsi 계산 로직 추가

--- src/main/java/com/dingdongdeng/coinautotrading/exchange/component/IndexCalculator.java
+++ src/main/java/com/dingdongdeng/coinautotrading/exchange/component/IndexCalculator.java
@@ -1,6 +1,8 @@
 package com.dingdongdeng.coinautotrading.exchange.component;
 
-import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
+import com.dingdongdeng.coinautotrading.exchange.service.model.ExchangeCandles;
+import com.dingdongdeng.coinautotrading.exchange.service.model.ExchangeCandles.Candle;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,9 +14,32 @@ public class IndexCalculator {
 
     private final int RSI_STANDARD_PERIOD = 14;
 
-    public double getRsi(CoinExchangeType coinExchangeType) {
+    public double getRsi(ExchangeCandles candles, Double currentPrice) {
+        List<Candle> candleList = candles.getCandleList().subList(0, RSI_STANDARD_PERIOD);
+        candleList.add(0, Candle.builder().tradePrice(currentPrice).build());
+
         double AU = 0d;
         double AD = 0d;
-        return 0d;
+        for (int i = 0; i < candleList.size(); i++) {
+            if (!hasNext(i, candleList.size())) {
+                break;
+            }
+            double todayPrice = candleList.get(i).getTradePrice();
+            double yesterdayPrice = candleList.get(i + 1).getTradePrice();
+
+            if (todayPrice > yesterdayPrice) {
+                AU += todayPrice - yesterdayPrice;
+            } else {
+                AD += yesterdayPrice - todayPrice;
+            }
+
+        }
+
+        double RS = AU / AD;
+        return RS / (1 + RS);
+    }
+
+    private boolean hasNext(int index, int size) {
+        return index + 1 < size;
     }
 }
