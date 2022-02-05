e1df56d3b8de54afbe48e5c44bce3853ff1ab0e0
DingDongDeng <etea583@naver.com>
2022-02-05 21:06:52 +0900

    TradingTask 생성자 추가

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/model/TradingTask.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/model/TradingTask.java
@@ -3,13 +3,17 @@ package com.dingdongdeng.coinautotrading.trading.strategy.model;
 import com.dingdongdeng.coinautotrading.common.type.CoinType;
 import com.dingdongdeng.coinautotrading.common.type.OrderType;
 import com.dingdongdeng.coinautotrading.common.type.PriceType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TradingTask {
 
     private CoinType coinType;
