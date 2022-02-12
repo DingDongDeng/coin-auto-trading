0c1297ba3ac0a4984fa5a18890b7bc4340bb22fe
DingDongDeng <etea583@naver.com>
2022-02-12 22:28:57 +0900

    RedisService 기본 expires 기간 설정

--- src/main/java/com/dingdongdeng/coinautotrading/common/redis/RedisService.java
+++ src/main/java/com/dingdongdeng/coinautotrading/common/redis/RedisService.java
@@ -1,5 +1,6 @@
 package com.dingdongdeng.coinautotrading.common.redis;
 
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
 public class RedisService {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final int DEFAULT_EXPIRE_DAYS = 3;
 
     public void set(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
+        redisTemplate.expire(key, DEFAULT_EXPIRE_DAYS, TimeUnit.DAYS);
     }
 
     public String get(String key) {
