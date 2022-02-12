df76493dd188e21a28b7e226b2ee1fdb58eea0f5
DingDongDeng <etea583@naver.com>
2022-02-13 01:12:14 +0900

    TradingResultRepository 생성

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/repository/TradingTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dingdongdeng.coinautotrading.trading.strategy.repository;
-
-import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingTask;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface TradingTaskRepository extends CrudRepository<TradingTask, String> {
-
-}
