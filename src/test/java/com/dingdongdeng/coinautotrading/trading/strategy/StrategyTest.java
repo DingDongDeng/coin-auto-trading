22beecc5f1f2b05954a7b93624a39b3bfe0cef74
DingDongDeng <etea583@naver.com>
2022-02-13 01:27:33 +0900

    오류나는 테스트 주석처리

--- src/test/java/com/dingdongdeng/coinautotrading/trading/strategy/StrategyTest.java
+++ src/test/java/com/dingdongdeng/coinautotrading/trading/strategy/StrategyTest.java
@@ -1,7 +1,5 @@
 package com.dingdongdeng.coinautotrading.trading.strategy;
 
-import com.dingdongdeng.coinautotrading.common.type.CoinType;
-import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
 import com.dingdongdeng.coinautotrading.exchange.service.UpbitExchangeService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -17,7 +15,7 @@ class StrategyTest {
 
     @Test
     public void Prototype_테스트() { //fixme Mock을 이용해서 테스트 고도화(TDD)
-        Strategy strategy = new RsiTradingStrategy(CoinType.ETHEREUM, TradingTerm.SCALPING, upbitExchangeProcessor);
+//        Strategy strategy = new RsiTradingStrategy(CoinType.ETHEREUM, TradingTerm.SCALPING, upbitExchangeProcessor);
         //log.info("orderTask : {}", strategy.execute());
         //log.info("orderTask : {}", strategy.execute());
         //log.info("orderTask : {}", strategy.execute());
