075cd078417e862119ed0ac60f455b592b205ad6
DingDongDeng <etea583@naver.com>
2022-02-13 01:26:20 +0900

    잘못된 import 수정

--- src/main/java/com/dingdongdeng/coinautotrading/domain/entity/TradeOrder.java
+++ src/main/java/com/dingdongdeng/coinautotrading/domain/entity/TradeOrder.java
@@ -9,13 +9,13 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
 
 @ToString
 @Getter
