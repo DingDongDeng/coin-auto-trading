0b60bb61baf545bbb49c040653168897378f708c
DingDongDeng <etea583@naver.com>
2022-02-22 22:28:18 +0900

    AutoTradingController에 userId를 받도록 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/autotrading/controller/AutoTradingController.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/autotrading/controller/AutoTradingController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,32 +21,32 @@ public class AutoTradingController {
     private final AutoTradingService autoTradingService;
 
     @PostMapping("/register")
-    public CommonResponse<Void> register(@RequestBody AutoTradingRegisterRequest request) {
-        autoTradingService.register(request);
+    public CommonResponse<Void> register(@RequestBody AutoTradingRegisterRequest request, @RequestHeader String userId) {
+        autoTradingService.register(request, userId);
         return CommonResponse.<Void>builder()
             .message("autotrading register success")
             .build();
     }
 
     @PostMapping("/{autoTradingProcessorId}/start")
-    public CommonResponse<Void> start(@PathVariable String autoTradingProcessorId) {
-        autoTradingService.start(autoTradingProcessorId);
+    public CommonResponse<Void> start(@PathVariable String autoTradingProcessorId, @RequestHeader String userId) {
+        autoTradingService.start(autoTradingProcessorId, userId);
         return CommonResponse.<Void>builder()
             .message("autotrading start success")
             .build();
     }
 
     @PostMapping("/{autoTradingProcessorId}/stop")
-    public CommonResponse<Void> stop(@PathVariable String autoTradingProcessorId) {
-        autoTradingService.stop(autoTradingProcessorId);
+    public CommonResponse<Void> stop(@PathVariable String autoTradingProcessorId, @RequestHeader String userId) {
+        autoTradingService.stop(autoTradingProcessorId, userId);
         return CommonResponse.<Void>builder()
             .message("autotrading stop success")
             .build();
     }
 
     @PostMapping("/{autoTradingProcessorId}/terminate")
-    public CommonResponse terminate(@PathVariable String autoTradingProcessorId) {
-        autoTradingService.terminate(autoTradingProcessorId);
+    public CommonResponse terminate(@PathVariable String autoTradingProcessorId, @RequestHeader String userId) {
+        autoTradingService.terminate(autoTradingProcessorId, userId);
         return CommonResponse.<Void>builder()
             .message("autotrading terminate success")
             .build();
