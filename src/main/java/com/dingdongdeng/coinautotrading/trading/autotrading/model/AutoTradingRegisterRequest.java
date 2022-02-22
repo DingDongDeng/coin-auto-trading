f3e9939685a1cd93b4d6bc84cb3f13bb6fe792ce
DingDongDeng <etea583@naver.com>
2022-02-22 23:11:36 +0900

    AutoTradingRegisterRequest test 코드 작성을 위한 생성자 롬복 어노테이션 추가

--- src/main/java/com/dingdongdeng/coinautotrading/trading/autotrading/model/AutoTradingRegisterRequest.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/autotrading/model/AutoTradingRegisterRequest.java
@@ -4,11 +4,17 @@ import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
 import com.dingdongdeng.coinautotrading.common.type.CoinType;
 import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.type.StrategyCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AutoTradingRegisterRequest {
 
     private String title;
