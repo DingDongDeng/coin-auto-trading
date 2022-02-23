d9e12640a55b60866bcfde45e84ffc34f8ccb2bd
DingDongDeng <etea583@naver.com>
2022-02-23 22:18:44 +0900

    ControllerExceptionHandler를 common 패키지로 이동하여 글로벌하게 사용

--- src/main/java/com/dingdongdeng/coinautotrading/common/exception/ControllerExceptionHandler.java
+++ src/main/java/com/dingdongdeng/coinautotrading/common/exception/ControllerExceptionHandler.java
@@ -1,11 +1,11 @@
-package com.dingdongdeng.coinautotrading.trading.autotrading.controller;
+package com.dingdongdeng.coinautotrading.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice(basePackages = "com.dingdongdeng.coinautotrading.trading.autotrading.controller")
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
