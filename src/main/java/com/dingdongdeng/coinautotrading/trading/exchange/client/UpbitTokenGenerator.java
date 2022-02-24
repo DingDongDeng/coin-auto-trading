e3f255b9bca93e26e7a8ffdf61b5f2450eaa3889
DingDongDeng <etea583@naver.com>
2022-02-24 21:35:35 +0900

    UpbitTokenGenerator가 ExchangeKeyService를 이용해 키값을 사용하도록 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/client/UpbitTokenGenerator.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/client/UpbitTokenGenerator.java
@@ -4,9 +4,12 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.dingdongdeng.coinautotrading.common.client.util.QueryParamsConverter;
-import com.dingdongdeng.coinautotrading.trading.exchange.client.config.UpbitClientResourceProperties;
+import com.dingdongdeng.coinautotrading.domain.entity.ExchangeKey;
+import com.dingdongdeng.coinautotrading.domain.service.ExchangeKeyService;
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +21,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class UpbitTokenGenerator {
 
-    private final UpbitClientResourceProperties properties;
+    private final String ACCESS_KEY_NAME = "ACCESS_KEY";
+    private final String SECRET_KEY_NAME = "SECRET_KEY";
+
+    private final ExchangeKeyService exchangeKeyService;
     private final QueryParamsConverter queryParamsConverter;
 
-    public String makeToken(Object request) {
-        String accessKey = properties.getAccessKey();
-        String secretKey = properties.getSecretKey();
+    public String makeToken(Object request, String keyPairId) {
+        List<ExchangeKey> exchangeKeyList = exchangeKeyService.findByPairId(keyPairId);
+        String accessKey = getKey(exchangeKeyList, ACCESS_KEY_NAME);
+        String secretKey = getKey(exchangeKeyList, SECRET_KEY_NAME);
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         JWTCreator.Builder jwtBuilder = JWT.create()
@@ -50,5 +57,13 @@ public class UpbitTokenGenerator {
         } catch (Exception e) {
             throw new RuntimeException("fail make query hash", e);
         }
+    }
+
+    private String getKey(List<ExchangeKey> exchangeKeyList, String keyName) {
+        return exchangeKeyList.stream()
+            .map(ExchangeKey::getName)
+            .filter(key -> key.equalsIgnoreCase(keyName))
+            .findFirst()
+            .orElseThrow(NoSuchElementException::new);
     }
 }
