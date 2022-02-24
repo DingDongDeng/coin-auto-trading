9d7b26d4d01b47bf4cba14091d5113d46a56210a
DingDongDeng <etea583@naver.com>
2022-02-24 22:49:52 +0900

    UpbitClientTest 수정

--- src/test/java/com/dingdongdeng/coinautotrading/trading/exchange/client/UpbitClientTest.java
+++ src/test/java/com/dingdongdeng/coinautotrading/trading/exchange/client/UpbitClientTest.java
@@ -2,7 +2,10 @@ package com.dingdongdeng.coinautotrading.trading.exchange.client;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
+import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
 import com.dingdongdeng.coinautotrading.common.type.CoinType;
+import com.dingdongdeng.coinautotrading.domain.entity.ExchangeKey;
+import com.dingdongdeng.coinautotrading.domain.repository.ExchangeKeyRepository;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitEnum.MarketType;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitEnum.OrdType;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitEnum.Side;
@@ -24,9 +27,12 @@ import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitRespo
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitResponse.TickerResponse;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
@@ -34,11 +40,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 class UpbitClientTest {
 
     @Autowired
+    private ExchangeKeyRepository exchangeKeyRepository;
+    @Autowired
     private UpbitClient upbitClient;
+    @Value("${upbit.client.accessKey}")
+    private String accessKey;
+    @Value("${upbit.client.secretKey}")
+    private String secretKey;
+
+    private String userId = "123456";
+    private String keyPairId;
+
+    @BeforeEach
+    public void setUp() {
+        String keyPairId = UUID.randomUUID().toString();
+        exchangeKeyRepository.save(
+            ExchangeKey.builder()
+                .pairId(keyPairId)
+                .coinExchangeType(CoinExchangeType.UPBIT)
+                .name("ACCESS_KEY")
+                .value(accessKey)
+                .userId(userId)
+                .build()
+        );
+
+        exchangeKeyRepository.save(
+            ExchangeKey.builder()
+                .pairId(keyPairId)
+                .coinExchangeType(CoinExchangeType.UPBIT)
+                .name("SECRET_KEY")
+                .value(secretKey)
+                .userId(userId)
+                .build()
+        );
+
+        this.keyPairId = keyPairId;
+
+    }
 
     @Test
     public void 전체_계좌_조회_테스트() {
-        List<AccountsResponse> response = upbitClient.getAccounts();
+        List<AccountsResponse> response = upbitClient.getAccounts(keyPairId);
         log.info("result : {}", response);
         AccountsResponse element = response.stream().findFirst().orElse(null);
         log.info("result of first element : {}", element);
@@ -49,7 +91,7 @@ class UpbitClientTest {
         MarketCodeRequest request = MarketCodeRequest.builder()
             .isDetail(true)
             .build();
-        List<MarketCodeResponse> response = upbitClient.getMarketList(request);
+        List<MarketCodeResponse> response = upbitClient.getMarketList(request, keyPairId);
         log.info("result : {}", response);
         MarketCodeResponse element = response.stream().findFirst().orElse(null);
         log.info("result of first element : {}", element);
@@ -60,7 +102,7 @@ class UpbitClientTest {
         OrderChanceRequest request = OrderChanceRequest.builder()
             .market(MarketType.of(CoinType.ETHEREUM).getCode())
             .build();
-        log.info("result : {}", upbitClient.getOrdersChance(request));
+        log.info("result : {}", upbitClient.getOrdersChance(request, keyPairId));
     }
 
     @Test
@@ -75,7 +117,7 @@ class UpbitClientTest {
             .limit(100)
             .orderBy("desc")
             .build();
-        List<OrderResponse> orderInfoListResponse = upbitClient.getOrderInfoList(orderInfoListRequest);
+        List<OrderResponse> orderInfoListResponse = upbitClient.getOrderInfoList(orderInfoListRequest, keyPairId);
         log.info("result orderInfoListResponse: {}", orderInfoListResponse);
     }
 
@@ -90,14 +132,14 @@ class UpbitClientTest {
             .price(5000.0)
             .ordType(OrdType.limit)
             .build();
-        OrderResponse orderResponse = upbitClient.order(orderRequest);
+        OrderResponse orderResponse = upbitClient.order(orderRequest, keyPairId);
         log.info("result orderResponse: {}", orderResponse);
 
         // 조회
         OrderInfoRequest orderInfoRequest = OrderInfoRequest.builder()
             .uuid(orderResponse.getUuid())
             .build();
-        OrderResponse orderInfoResponse = upbitClient.getOrderInfo(orderInfoRequest);
+        OrderResponse orderInfoResponse = upbitClient.getOrderInfo(orderInfoRequest, keyPairId);
         log.info("result orderInfoResponse: {}", orderInfoResponse);
 
         // 리스트 조회
@@ -111,14 +153,14 @@ class UpbitClientTest {
             .limit(100)
             .orderBy("desc")
             .build();
-        List<OrderResponse> orderInfoListResponse = upbitClient.getOrderInfoList(orderInfoListRequest);
+        List<OrderResponse> orderInfoListResponse = upbitClient.getOrderInfoList(orderInfoListRequest, keyPairId);
         log.info("result orderInfoListResponse: {}", orderInfoListResponse);
 
         // 취소
         OrderCancelRequest orderCancelRequest = OrderCancelRequest.builder()
             .uuid(orderResponse.getUuid())
             .build();
-        log.info("result orderCancelResponse: {}", upbitClient.orderCancel(orderCancelRequest));
+        log.info("result orderCancelResponse: {}", upbitClient.orderCancel(orderCancelRequest, keyPairId));
     }
 
     @Test
@@ -132,7 +174,7 @@ class UpbitClientTest {
             .count(2)
             .build();
 
-        List<CandleResponse> response = upbitClient.getMinuteCandle(request);
+        List<CandleResponse> response = upbitClient.getMinuteCandle(request, keyPairId);
         log.info("result : {}", response);
     }
 
@@ -141,7 +183,8 @@ class UpbitClientTest {
         List<OrderBookResponse> response = upbitClient.getOrderBook(
             OrderBookRequest.builder()
                 .marketList(List.of(MarketType.KRW_ETH.getCode()))
-                .build()
+                .build(),
+            keyPairId
         );
         log.info("result : {}", response);
     }
@@ -151,7 +194,8 @@ class UpbitClientTest {
         List<TickerResponse> response = upbitClient.getTicker(
             TickerRequest.builder()
                 .marketList(List.of(MarketType.KRW_ETH.getCode()))
-                .build()
+                .build(),
+            keyPairId
         );
         log.info("result : {}", response);
     }
