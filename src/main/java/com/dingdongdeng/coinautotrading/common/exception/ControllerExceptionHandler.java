c32742b94fac551b2484f35e625463bec8c7a35c
DingDongDeng <etea583@naver.com>
2022-02-26 22:01:26 +0900

    ControllerExceptionHandler의 httpStatus 응답값 수정

--- src/main/java/com/dingdongdeng/coinautotrading/common/exception/ControllerExceptionHandler.java
+++ src/main/java/com/dingdongdeng/coinautotrading/common/exception/ControllerExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.dingdongdeng.coinautotrading.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //fixme status 처리 고도화 필요
     @ExceptionHandler(Exception.class)
     public String exceptionHandler(Exception e) {
         log.error(e.getMessage(), e);
