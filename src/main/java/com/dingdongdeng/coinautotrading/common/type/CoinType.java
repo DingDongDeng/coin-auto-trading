fb8fa49bdb667d7b2368c8a04e842a75caac6eff
TaeJoon Jeon <46275680+DingDongDeng@users.noreply.github.com>
2022-03-03 20:20:10 +0900

    거래 가능 코인 종류 추가 (#20)

--- src/main/java/com/dingdongdeng/coinautotrading/common/type/CoinType.java
+++ src/main/java/com/dingdongdeng/coinautotrading/common/type/CoinType.java
@@ -7,11 +7,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CoinType {
-    ETHEREUM("이더리움"),
     ADA("에이다"),
+    SOLANA("솔라나"),
     XRP("리플"),
+    ETHEREUM("이더리움"),
+    AVALANCHE("아발란체"),
+    POLKADOT("폴카닷"),
     DOGE("도지"),
+    BITCOIN("비트코인"),
     ;
+
     private String desc;
 
     public static EnumMap<CoinType, String> toMap() {
