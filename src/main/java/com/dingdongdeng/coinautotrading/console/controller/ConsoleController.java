a626cf9b0eaee42e297a7397650d285978950669
DingDongDeng <etea583@naver.com>
2022-03-09 18:07:56 +0900

    발송 테스트 코드 제거

--- src/main/java/com/dingdongdeng/coinautotrading/console/controller/ConsoleController.java
+++ src/main/java/com/dingdongdeng/coinautotrading/console/controller/ConsoleController.java
@@ -1,13 +1,11 @@
 package com.dingdongdeng.coinautotrading.console.controller;
 
 import com.dingdongdeng.coinautotrading.common.model.CommonResponse;
-import com.dingdongdeng.coinautotrading.common.slack.SlackSender;
 import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
 import com.dingdongdeng.coinautotrading.common.type.CoinType;
 import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
 import com.dingdongdeng.coinautotrading.console.controller.model.TypeInfoResponse;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.type.StrategyCode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ConsoleController {
 
-    @Autowired
-    private SlackSender slackSender;
-
     @RequestMapping("/")
     public String index() {
-        slackSender.send(new RuntimeException("발송테스트"));
         return "index";
     }
 
