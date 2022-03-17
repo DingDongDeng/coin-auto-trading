0e5f92b5ff159b33eb291815ebee989aa05ae8fb
DingDongDeng <etea583@naver.com>
2022-03-17 21:42:43 +0900

    Strategy 상속 관계제거 및 이에 따른 필드 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/Strategy.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/Strategy.java
@@ -1,15 +1,7 @@
 package com.dingdongdeng.coinautotrading.trading.strategy;
 
-import com.dingdongdeng.coinautotrading.common.type.CoinType;
 import com.dingdongdeng.coinautotrading.common.type.OrderType;
-import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
-import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeService;
-import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeOrder;
-import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeOrderCancel;
-import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeOrderCancelParam;
-import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeOrderParam;
 import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeTradingInfo;
-import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeTradingInfoParam;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingResult;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingTask;
 import java.util.List;
@@ -18,45 +10,38 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-public abstract class Strategy {
+public class Strategy {
 
-    private final CoinType coinType;
-    private final TradingTerm tradingTerm;
-    private final String keyPairId;
-    private final ExchangeService exchangeService;
+    private final StrategyCore strategyCore;
+    private final StrategyService strategyService;
+    private final StrategyOrderInfoStroe strategyOrderInfoStroe;
 
     public void execute() {
 
-        ExchangeTradingInfo exchangeTradingInfo = exchangeService.getTradingInformation(makeExchangeTradingInfoParam(), keyPairId);
+        ExchangeTradingInfo exchangeTradingInfo = strategyService.getTradingInformation();
 
-        List<TradingTask> tradingTaskList = this.makeTradingTask(exchangeTradingInfo);
+        List<TradingTask> tradingTaskList = strategyCore.makeTradingTask(exchangeTradingInfo);
         log.info("tradingTaskList : {}", tradingTaskList);
 
         tradingTaskList.forEach(tradingTask -> {
             // 매수, 매도 주문
             if (isOrder(tradingTask)) {
-                ExchangeOrder order = exchangeService.order(makeExchangeOrderParam(tradingTask), keyPairId);
-                this.handleOrderResult(order, makeTradingResult(tradingTask, order));
+                TradingResult orderTradingResult = strategyService.order(tradingTask);
+                strategyOrderInfoStroe.storeTradingResult(orderTradingResult); // 주문 성공 건 정보 저장
+                strategyCore.handleOrderResult(orderTradingResult);
                 return;
             }
 
             // 주문 취소
             if (isOrderCancel(tradingTask)) {
-                ExchangeOrderCancel orderCancel = exchangeService.orderCancel(makeExchangeOrderCancelParam(tradingTask), keyPairId);
-                this.handleOrderCancelResult(orderCancel, makeTradingResult(tradingTask, orderCancel));
+                TradingResult cancelTradingResult = strategyService.orderCancel(tradingTask);
+                strategyOrderInfoStroe.reset(cancelTradingResult); // 주문 취소 건 정보 제거
+                strategyCore.handleOrderCancelResult(cancelTradingResult);
             }
 
             // 아무것도 하지 않음
         });
     }
-
-    abstract public String getIdentifyCode();
-
-    abstract protected List<TradingTask> makeTradingTask(ExchangeTradingInfo tradingInfo);
-
-    abstract protected void handleOrderResult(ExchangeOrder order, TradingResult tradingResult);
-
-    abstract protected void handleOrderCancelResult(ExchangeOrderCancel orderCancel, TradingResult tradingResult);
 
     private boolean isOrder(TradingTask tradingTask) {
         OrderType orderType = tradingTask.getOrderType();
@@ -68,58 +53,4 @@ public abstract class Strategy {
         return orderType == OrderType.CANCEL;
     }
 
-    private ExchangeTradingInfoParam makeExchangeTradingInfoParam() {
-        return ExchangeTradingInfoParam.builder()
-            .coinType(coinType)
-            .tradingTerm(tradingTerm)
-            .build();
-    }
-
-    private ExchangeOrderParam makeExchangeOrderParam(TradingTask tradingTask) {
-        return ExchangeOrderParam.builder()
-            .coinType(tradingTask.getCoinType())
-            .orderType(tradingTask.getOrderType())
-            .volume(tradingTask.getVolume())
-            .price(tradingTask.getPrice())
-            .priceType(tradingTask.getPriceType())
-            .build();
-    }
-
-    private ExchangeOrderCancelParam makeExchangeOrderCancelParam(TradingTask tradingTask) {
-        return ExchangeOrderCancelParam.builder()
-            .orderId(tradingTask.getOrderId())
-            .build();
-    }
-
-    private TradingResult makeTradingResult(TradingTask tradingTask, ExchangeOrder exchangeOrder) {
-        return TradingResult.builder()
-            .identifyCode(tradingTask.getIdentifyCode())
-            .coinType(tradingTask.getCoinType())
-            .orderType(tradingTask.getOrderType())
-            .tradingTerm(tradingTask.getTradingTerm())
-            .orderState(exchangeOrder.getOrderState())
-            .volume(tradingTask.getVolume())
-            .price(tradingTask.getPrice())
-            .priceType(tradingTask.getPriceType())
-            .orderId(exchangeOrder.getOrderId())
-            .tag(tradingTask.getTag())
-            .createdAt(exchangeOrder.getCreatedAt())
-            .build();
-    }
-
-    private TradingResult makeTradingResult(TradingTask tradingTask, ExchangeOrderCancel exchangeOrderCancel) { //fixme 중복 코드 개선
-        return TradingResult.builder()
-            .identifyCode(tradingTask.getIdentifyCode())
-            .coinType(tradingTask.getCoinType())
-            .orderType(tradingTask.getOrderType())
-            .tradingTerm(tradingTask.getTradingTerm())
-            .orderState(exchangeOrderCancel.getOrderState())
-            .volume(tradingTask.getVolume())
-            .price(tradingTask.getPrice())
-            .priceType(tradingTask.getPriceType())
-            .orderId(exchangeOrderCancel.getOrderId())
-            .tag(tradingTask.getTag())
-            .createdAt(exchangeOrderCancel.getCreatedAt())
-            .build();
-    }
 }
