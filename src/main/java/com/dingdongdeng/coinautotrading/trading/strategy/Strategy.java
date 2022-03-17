84325c1adf3a7c21bfdd5249076b1b96d2b3c1c9
DingDongDeng <etea583@naver.com>
2022-03-17 22:38:27 +0900

    Strategy에 reset 테스크 처리하는 로직추가하고, identifyCode 생성자 추가

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/Strategy.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/Strategy.java
@@ -4,7 +4,9 @@ import com.dingdongdeng.coinautotrading.common.type.OrderType;
 import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeTradingInfo;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingResult;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingTask;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.type.StrategyCode;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,18 +14,30 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class Strategy {
 
+    private final String identifyCode;
     private final StrategyCore strategyCore;
     private final StrategyService strategyService;
     private final StrategyOrderInfoStore strategyOrderInfoStore;
 
+    public Strategy(StrategyCode code, StrategyCore core, StrategyService service, StrategyOrderInfoStore orderInfoStore) {
+        this.identifyCode = code.name() + UUID.randomUUID().toString();
+        this.strategyCore = core;
+        this.strategyService = service;
+        this.strategyOrderInfoStore = orderInfoStore;
+    }
+
     public void execute() {
-
-        ExchangeTradingInfo exchangeTradingInfo = strategyService.getTradingInformation();
-
+        ExchangeTradingInfo exchangeTradingInfo = strategyService.getTradingInformation(identifyCode);
         List<TradingTask> tradingTaskList = strategyCore.makeTradingTask(exchangeTradingInfo);
         log.info("tradingTaskList : {}", tradingTaskList);
 
         tradingTaskList.forEach(tradingTask -> {
+            // 모든 정보 초기화
+            if (isReset(tradingTask)) {
+                strategyOrderInfoStore.reset(identifyCode);
+                return;
+            }
+
             // 매수, 매도 주문
             if (isOrder(tradingTask)) {
                 TradingResult orderTradingResult = strategyService.order(tradingTask);
@@ -37,10 +51,16 @@ public class Strategy {
                 TradingResult cancelTradingResult = strategyService.orderCancel(tradingTask);
                 strategyOrderInfoStore.reset(cancelTradingResult); // 주문 취소 건 정보 제거
                 strategyCore.handleOrderCancelResult(cancelTradingResult);
+                return;
             }
 
             // 아무것도 하지 않음
+            log.info("do nothing");
         });
+    }
+
+    private boolean isReset(TradingTask tradingTask) {
+        return tradingTask.isReset();
     }
 
     private boolean isOrder(TradingTask tradingTask) {
