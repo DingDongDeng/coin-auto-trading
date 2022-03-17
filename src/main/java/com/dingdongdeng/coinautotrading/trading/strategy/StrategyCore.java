5acb1ace9d9110050360a9b76b18aa1e2f6dbe95
DingDongDeng <etea583@naver.com>
2022-03-17 23:32:58 +0900

    StrategyCore.makeTradingTask 메소드가 TradingInfo 를 사용하도록 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/StrategyCore.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/StrategyCore.java
@@ -1,13 +1,13 @@
 package com.dingdongdeng.coinautotrading.trading.strategy;
 
-import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeTradingInfo;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingInfo;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingResult;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingTask;
 import java.util.List;
 
 public interface StrategyCore {
 
-    List<TradingTask> makeTradingTask(ExchangeTradingInfo tradingInfo);
+    List<TradingTask> makeTradingTask(TradingInfo tradingInfo);
 
     void handleOrderResult(TradingResult tradingResult);
 
