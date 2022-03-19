9bcee7b7d98029fbf5e4ad0dfacde779b55cfa3f
DingDongDeng <etea583@naver.com>
2022-03-19 21:33:40 +0900

    BackTestingService 필드에 ExchangeService 추가하고 doTest 메소드 로직에 반영

--- src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/service/BackTestingService.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/service/BackTestingService.java
@@ -1,9 +1,12 @@
 package com.dingdongdeng.coinautotrading.trading.backtesting.service;
 
+import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
 import com.dingdongdeng.coinautotrading.trading.autotrading.model.AutoTradingProcessor;
 import com.dingdongdeng.coinautotrading.trading.backtesting.model.BackTestingProcessor;
 import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeCandleService;
 import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeCandleServiceSelector;
+import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeService;
+import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeServiceSelector;
 import com.dingdongdeng.coinautotrading.trading.index.IndexCalculator;
 import com.dingdongdeng.coinautotrading.trading.strategy.Strategy;
 import com.dingdongdeng.coinautotrading.trading.strategy.StrategyFactory;
@@ -19,17 +22,27 @@ import org.springframework.stereotype.Service;
 public class BackTestingService {
 
     private final StrategyFactory strategyFactory;
-    private final ExchangeCandleServiceSelector candleServiceSelector;
+    private final ExchangeCandleServiceSelector exchangeCandleServiceSelector;
+    private final ExchangeServiceSelector exchangeServiceSelector;
     private final IndexCalculator indexCalculator;
 
     public BackTestingProcessor doTest(AutoTradingProcessor autoTradingProcessor, LocalDateTime start, LocalDateTime end) {
 
-        ExchangeCandleService candleService = candleServiceSelector.getTargetService(autoTradingProcessor.getCoinExchangeType());
-        BackTestingContextLoader contextLoader = new BackTestingContextLoader(candleService, start, end);
+        CoinExchangeType coinExchangeType = autoTradingProcessor.getCoinExchangeType();
+        ExchangeService exchangeService = exchangeServiceSelector.getTargetService(coinExchangeType);
+        ExchangeCandleService exchangeCandleService = exchangeCandleServiceSelector.getTargetService(coinExchangeType);
+
+        BackTestingContextLoader contextLoader = new BackTestingContextLoader(exchangeCandleService, start, end);
+
+        BackTestingExchangeService backTestingExchangeService = BackTestingExchangeService.builder()
+            .contextLoader(contextLoader)
+            .exchangeService(exchangeService)
+            .indexCalculator(indexCalculator)
+            .build();
 
         Strategy backTestingStrategy = strategyFactory.create(
             autoTradingProcessor.getStrategy().getStrategyCode(),
-            new BackTestingExchangeService(contextLoader, indexCalculator),
+            backTestingExchangeService,
             autoTradingProcessor.getCoinType(),
             autoTradingProcessor.getTradingTerm(),
             null
