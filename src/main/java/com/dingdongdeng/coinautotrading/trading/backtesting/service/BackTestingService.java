00bca7bdf464c681ebb6ff2b062557e47f6013cf
DingDongDeng <etea583@naver.com>
2022-03-19 21:03:30 +0900

    BackTestingService.doTest 구현

--- src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/service/BackTestingService.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/service/BackTestingService.java
@@ -1,6 +1,12 @@
 package com.dingdongdeng.coinautotrading.trading.backtesting.service;
 
-import com.dingdongdeng.coinautotrading.trading.backtesting.model.BackTestingRequest;
+import com.dingdongdeng.coinautotrading.trading.autotrading.model.AutoTradingProcessor;
+import com.dingdongdeng.coinautotrading.trading.backtesting.model.BackTestingProcessor;
+import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeCandleService;
+import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeCandleServiceSelector;
+import com.dingdongdeng.coinautotrading.trading.strategy.Strategy;
+import com.dingdongdeng.coinautotrading.trading.strategy.StrategyFactory;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,11 +17,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class BackTestingService {
 
-    public String doTest(BackTestingRequest.Register request) {
-        return UUID.randomUUID().toString(); // backTestingId
-    }
+    private final StrategyFactory strategyFactory;
+    private final ExchangeCandleServiceSelector candleServiceSelector;
 
-    public Object getResult(String backTestingId) {
-        return null;
+    public BackTestingProcessor doTest(AutoTradingProcessor autoTradingProcessor, LocalDateTime start, LocalDateTime end) {
+
+        ExchangeCandleService candleService = candleServiceSelector.getTargetService(autoTradingProcessor.getCoinExchangeType());
+        BackTestingContextLoader contextLoader = new BackTestingContextLoader(candleService, start, end);
+
+        Strategy backTestingStrategy = strategyFactory.create(
+            autoTradingProcessor.getStrategy().getStrategyCode(),
+            new BackTestingExchangeService(contextLoader),
+            autoTradingProcessor.getCoinType(),
+            autoTradingProcessor.getTradingTerm(),
+            null
+        );
+
+        BackTestingProcessor backTestingProcessor = BackTestingProcessor.builder()
+            .id(UUID.randomUUID().toString())
+            .backTestingStrategy(backTestingStrategy)
+            .contextLoader(contextLoader)
+            .build();
+
+        backTestingProcessor.start();
+
+        return backTestingProcessor;
     }
 }
