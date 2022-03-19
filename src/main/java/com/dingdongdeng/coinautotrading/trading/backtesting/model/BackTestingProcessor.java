75c8972baee2cd77cb126925f0ee73d4b8f0c64a
DingDongDeng <etea583@naver.com>
2022-03-19 16:17:41 +0900

    BackTestingProcessor 스켈레톤 생성

---
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/model/BackTestingProcessor.java
@@ -0,0 +1,27 @@
+package com.dingdongdeng.coinautotrading.trading.backtesting.model;
+
+import com.dingdongdeng.coinautotrading.trading.strategy.Strategy;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ToString
+@Getter
+@AllArgsConstructor
+@Builder
+public class BackTestingProcessor {
+
+    @Default
+    private String id = UUID.randomUUID().toString();
+    private Strategy strategy;
+
+    public void start() {
+
+        //fixme async
+    }
+}
