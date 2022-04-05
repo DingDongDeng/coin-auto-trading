d71178a0f4c09d8c4c060b806c7b535f35f33425
DingDongDeng <tpea999@gmail.com>
2022-04-05 22:28:48 +0900

    백테스팅 할때 현재 캔들이 누락되거나 중복하여 들어가는 현상 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/context/BackTestingContextLoader.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/backtesting/context/BackTestingContextLoader.java
@@ -1,7 +1,10 @@
 package com.dingdongdeng.coinautotrading.trading.backtesting.context;
 
 import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
+import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeCandles;
 import com.dingdongdeng.coinautotrading.trading.exchange.service.model.ExchangeCandles.Candle;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -39,7 +42,25 @@ public class BackTestingContextLoader {
             .coinType(candleLoader.getCoinType())
             .currentPrice(candle.getTradePrice())
             .now(candle.getCandleDateTimeKst())
-            .candles(candleLoader.getCandles(tradingTerm.getCandleUnit(), null, candle.getCandleDateTimeKst())) //fixme 백테스팅 속도가 너무 느려져 개선필요
+            .candles(getCandles(candle.getTradePrice(), candle.getCandleDateTimeKst()))
             .build();
+    }
+
+    private ExchangeCandles getCandles(Double currentPrice, LocalDateTime currentTime) {
+        ExchangeCandles candles = candleLoader.getCandles(tradingTerm.getCandleUnit(), null, currentTime); //fixme 백테스팅 속도가 너무 느려져 개선필요
+        List<Candle> candleList = candles.getCandleList();
+        if (!isExistCurrentCandle(currentTime, candleList)) {
+            candleList.add(
+                Candle.builder()
+                    .candleDateTimeKst(currentTime)
+                    .tradePrice(currentPrice)
+                    .build()
+            );
+        }
+        return candles;
+    }
+
+    private boolean isExistCurrentCandle(LocalDateTime currentTime, List<Candle> candleList) {
+        return candleList.get(candleList.size() - 1).getCandleDateTimeKst().equals(currentTime);
     }
 }
