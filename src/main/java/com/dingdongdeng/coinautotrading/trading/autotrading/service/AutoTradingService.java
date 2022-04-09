91b6a93e03fc4ac452e4e99407a6bccfdd62ffde
DingDongDeng <tpea999@gmail.com>
2022-04-09 13:23:54 +0900

    AutoTradingService의 StrategyCoreParam 생성 로직 추가

--- src/main/java/com/dingdongdeng/coinautotrading/trading/autotrading/service/AutoTradingService.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/autotrading/service/AutoTradingService.java
@@ -8,6 +8,8 @@ import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeService
 import com.dingdongdeng.coinautotrading.trading.exchange.service.ExchangeServiceSelector;
 import com.dingdongdeng.coinautotrading.trading.strategy.Strategy;
 import com.dingdongdeng.coinautotrading.trading.strategy.StrategyFactory;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.StrategyCoreParam;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.StrategyServiceParam;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +32,27 @@ public class AutoTradingService {
 
     public AutoTradingProcessor register(AutoTradingRegisterRequest request, String userId) {
         ExchangeService exchangeService = processorSelector.getTargetService(request.getCoinExchangeType());
-        Strategy strategy = strategyFactory.create(request.getStrategyCode(), exchangeService, request.getCoinType(), request.getTradingTerm(), request.getKeyPairId());
+        StrategyServiceParam serviceParam = StrategyServiceParam.builder()
+            .strategyCode(request.getStrategyCode())
+            .exchangeService(exchangeService)
+            .coinType(request.getCoinType())
+            .tradingTerm(request.getTradingTerm())
+            .keyPairId(request.getKeyPairId())
+            .build();
+
+        //fixme 클라이언트 input과 실제 StrategyCore클래스의 관계를 동적으로 표현할 수 있는 구조를 고민해보자
+        StrategyCoreParam coreParam = StrategyCoreParam.builder()
+            .buyRsi(request.getBuyRsi())
+            .profitRsi(request.getProfitRsi())
+            .lossRsi(request.getLossRsi())
+            .profitLimitPriceRate(request.getProfitLimitPriceRate())
+            .lossLimitPriceRate(request.getLossLimitPriceRate())
+            .tooOldOrderTimeSeconds(request.getTooOldOrderTimeSeconds())
+            .orderPrice(request.getOrderPrice())
+            .accountBalanceLimit(request.getAccountBalanceLimit())
+            .build();
+
+        Strategy strategy = strategyFactory.create(serviceParam, coreParam);
         AutoTradingProcessor processor = autoTradingManager.register(
             AutoTradingProcessor.builder()
                 .id(UUID.randomUUID().toString())
