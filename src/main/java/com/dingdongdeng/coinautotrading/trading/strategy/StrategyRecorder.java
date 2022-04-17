61c46eaebc6b3e219d144a7492c76628483d7dc2
DingDongDeng <tpea999@gmail.com>
2022-04-17 17:04:05 +0900

    StrategyRecorder 수수료 관련 필드 추가

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/StrategyRecorder.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/StrategyRecorder.java
@@ -12,7 +12,7 @@ public class StrategyRecorder {
     private double totalBuyPrice;
     private double totalProfitPrice;
     private double totalLossPrice;
-    private double totalPaidFee;
+    private double totalFee;
     private double marginPrice;
     private double marginRate;
     private String eventMessage = ""; //fixme 메모리 이슈 가능성이 있음
@@ -20,7 +20,7 @@ public class StrategyRecorder {
     public void apply(TradingResult tradingResult) {
         addEventMessage("주문", tradingResult);
 
-        totalPaidFee += tradingResult.getFee();
+        totalFee += tradingResult.getFee();
         TradingTag tag = tradingResult.getTag();
         if (tag == TradingTag.BUY) {
             totalBuyPrice += tradingResult.getPrice() * tradingResult.getVolume();
@@ -38,7 +38,7 @@ public class StrategyRecorder {
     public void revert(TradingResult tradingResult) {
         addEventMessage("취소", tradingResult);
 
-        totalPaidFee -= tradingResult.getFee();
+        totalFee -= tradingResult.getFee();
         TradingTag tag = tradingResult.getTag();
         if (tag == TradingTag.BUY) {
             totalBuyPrice -= tradingResult.getPrice() * tradingResult.getVolume();
@@ -54,8 +54,8 @@ public class StrategyRecorder {
     }
 
     private void calcMargin() {
-        this.marginRate = ((totalProfitPrice + totalLossPrice - totalPaidFee) / totalBuyPrice) * 100d - 100d;
-        this.marginPrice = (totalProfitPrice + totalLossPrice - totalPaidFee) - totalBuyPrice;
+        this.marginRate = ((totalProfitPrice + totalLossPrice - totalFee) / totalBuyPrice) * 100d - 100d;
+        this.marginPrice = (totalProfitPrice + totalLossPrice - totalFee) - totalBuyPrice;
     }
 
     private void addEventMessage(String event, TradingResult tradingResult) {
