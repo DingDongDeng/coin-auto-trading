2508d55a819c2f16620c78442482f3a879f4084b
DingDongDeng <tpea999@gmail.com>
2022-04-22 00:30:27 +0900

    username -> userId로 파라미터명 바꾸고, 로그인 성공 핸들러를 추가

--- src/main/java/com/dingdongdeng/coinautotrading/auth/config/SecurityConfig.java
+++ src/main/java/com/dingdongdeng/coinautotrading/auth/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.dingdongdeng.coinautotrading.auth.config;
 
 import com.dingdongdeng.coinautotrading.auth.service.UserService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -31,10 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
             .formLogin()
             .loginPage("/login")
-            .usernameParameter("username")
+            .usernameParameter("userId")
             .passwordParameter("password")
             .loginProcessingUrl("/login")
-            .defaultSuccessUrl("/")
+            .successHandler(new LoginSuccessHandler())
             .and()
 
             .logout()
@@ -48,14 +56,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .disable();
     }
 
-    /**
-     * 로그인 인증 처리 메소드
-     *
-     * @param auth
-     * @throws Exception
-     */
-    @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    private class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            HttpSession session = request.getSession();
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            session.setAttribute("userId", userDetails.getUsername());
+            response.sendRedirect("/");
+        }
     }
 }
