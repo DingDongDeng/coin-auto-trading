f7177618e0cae8b85966690992bd1db09c1e4788
DingDongDeng <tpea999@gmail.com>
2022-04-30 14:00:43 +0900

    ScaleTradingRsiStrategyCore 로직 오류 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ScaleTradingRsiStrategyCore.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ScaleTradingRsiStrategyCore.java
@@ -134,8 +134,6 @@ public class ScaleTradingRsiStrategyCore implements StrategyCore {
                         .build()
                 );
             }
-
-            return List.of(new TradingTask());
         }
 
         /*
