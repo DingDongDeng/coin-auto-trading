6de077c87d93ad62f8edb4c28f78083f5e1d55cd
DingDongDeng <tpea999@gmail.com>
2022-05-01 16:12:23 +0900

    오타 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ScaleTradingRsiStrategyCore2.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ScaleTradingRsiStrategyCore2.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ScaleTradingRsiStrategyCore2 implements StrategyCore {
 
-    private final ScaleTradingRsiStrategyCoreParam param;
+    private final ScaleTradingRsiStrategyCoreParam2 param;
 
     @Override
     public List<TradingTask> makeTradingTask(TradingInfo tradingInfo) {
