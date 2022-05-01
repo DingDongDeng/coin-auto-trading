c0aa16eb31938db96062ceb97339f14f811e51d3
DingDongDeng <tpea999@gmail.com>
2022-05-01 12:14:53 +0900

    ScaleTradingRsiStrategyCore를  분할 매매 로직을 별도 클래스로 생성(ScaleTradingRsiStrategyCore2)

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/model/type/StrategyCode.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/model/type/StrategyCode.java
@@ -4,7 +4,9 @@ import com.dingdongdeng.coinautotrading.trading.strategy.StrategyCore;
 import com.dingdongdeng.coinautotrading.trading.strategy.core.RsiStrategyCore;
 import com.dingdongdeng.coinautotrading.trading.strategy.core.RsiStrategyCoreParam;
 import com.dingdongdeng.coinautotrading.trading.strategy.core.ScaleTradingRsiStrategyCore;
+import com.dingdongdeng.coinautotrading.trading.strategy.core.ScaleTradingRsiStrategyCore2;
 import com.dingdongdeng.coinautotrading.trading.strategy.core.ScaleTradingRsiStrategyCoreParam;
+import com.dingdongdeng.coinautotrading.trading.strategy.core.ScaleTradingRsiStrategyCoreParam2;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.StrategyCoreParam;
 import java.util.Arrays;
 import java.util.EnumMap;
@@ -17,6 +19,7 @@ import lombok.Getter;
 public enum StrategyCode {
     RSI("RSI 지표 기반 매매", RsiStrategyCore.class, RsiStrategyCoreParam.class),
     SCALE_TRADING_RSI("RSI 지표 기반 물타기 매매", ScaleTradingRsiStrategyCore.class, ScaleTradingRsiStrategyCoreParam.class),
+    SCALE_TRADING_RSI2("RSI 지표 기반 물타기 매매(분할 손절)", ScaleTradingRsiStrategyCore2.class, ScaleTradingRsiStrategyCoreParam2.class),
     ;
 
     private String desc;
