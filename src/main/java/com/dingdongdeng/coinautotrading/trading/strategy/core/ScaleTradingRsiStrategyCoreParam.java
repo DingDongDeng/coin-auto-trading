e84343ce40fe713b68dfb1c20530605b28f76752
DingDongDeng <tpea999@gmail.com>
2022-05-15 23:12:10 +0900

    가장 성적이 좋았던 설정값으로 파라미터 설명을 변경

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ScaleTradingRsiStrategyCoreParam.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ScaleTradingRsiStrategyCoreParam.java
@@ -27,15 +27,15 @@ public class ScaleTradingRsiStrategyCoreParam implements StrategyCoreParam {
     @GuideMessage("미체결 주문 취소를 위한 대기 시간(second)을 입력해주세요. ex) 30")
     private int tooOldOrderTimeSeconds;  // 초(second)
 
-    @GuideMessage("최초 주문할 금액을 입력해주세요. ex) 5000")
+    @GuideMessage("최초 주문할 금액을 입력해주세요. ex) 40000")
     private double orderPrice; // 한번에 주문할 금액
 
     @GuideMessage("계좌 안전 금액을 입력해주세요.")
     private double accountBalanceLimit;  //계좌 금액 안전 장치
 
-    @GuideMessage("분할 매수를 할 손실율(0.05을 설정하면 손실율이 5%가 될때마다 매수)")
+    @GuideMessage("분할 매수를 할 손실율(0.10을 설정하면 손실율이 10%가 될때마다 매수)")
     private double buyLossRate;
 
-    @GuideMessage("분할 매수를 할때 수량 비율(2를 설정하면 분할 매수할때마다 보유 물량의 2배를 매수)")
+    @GuideMessage("분할 매수를 할때 수량 비율(1를 설정하면 분할 매수할때마다 보유 물량의 1배를 매수)")
     private double buyVolumeRate;
 }
