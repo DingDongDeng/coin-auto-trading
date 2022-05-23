ca56a1e7c84fd995ac4166c9ff7de3bee2ed0d5b
TaeJoon Jeon <46275680+DingDongDeng@users.noreply.github.com>
2022-05-23 22:58:16 +0900

    Client 관련 클래스 구조 개선 (#76)

--- src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/client/BinanceFutureClient.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/client/BinanceFutureClient.java
@@ -1,37 +1,52 @@
 package com.dingdongdeng.coinautotrading.trading.exchange.client;
 
-import com.dingdongdeng.coinautotrading.common.client.Client;
+import com.dingdongdeng.coinautotrading.common.client.ResponseHandler;
 import com.dingdongdeng.coinautotrading.common.client.util.QueryParamsConverter;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FuturesAccountBalanceRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.FutureAccountBalanceResponse;
 import java.util.List;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
+@RequiredArgsConstructor
 @Component
-public class BinanceFutureClient extends Client {
+public class BinanceFutureClient {
 
+    private final WebClient binanceFutureWebClient;
     private final BinanceFutureTokenGenerator tokenGenerator;
     private final BinanceFutureSignatureWrapper signatureWrapper;
 
-    public BinanceFutureClient(WebClient binanceFutureWebClient, BinanceFutureTokenGenerator tokenGenerator, BinanceFutureSignatureWrapper signatureWrapper, QueryParamsConverter queryParamsConverter) {
-        super(binanceFutureWebClient, queryParamsConverter);
-        this.tokenGenerator = tokenGenerator;
-        this.signatureWrapper = signatureWrapper;
-    }
+    private final QueryParamsConverter queryParamsConverter;
+    private final ResponseHandler responseHandler;
 
     public String getServerTime() {
-        return get("/fapi/v1/time", new ParameterizedTypeReference<>() {
-        }, new HttpHeaders());
+        return responseHandler.handle(
+            () -> binanceFutureWebClient.get()
+                .uri("/fapi/v1/time")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block()
+        );
     }
 
     public List<FutureAccountBalanceResponse> getFuturesAccountBalance(FuturesAccountBalanceRequest request, String keyPairId) {
-        return get("/fapi/v2/balance", makeSignatureWrapper(request, keyPairId), new ParameterizedTypeReference<>() {
-        }, makeHeaders(keyPairId));
+        return responseHandler.handle(
+            () -> binanceFutureWebClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/fapi/v2/balance")
+                    .queryParams(queryParamsConverter.convertMap(makeSignatureWrapper(request, keyPairId)))
+                    .build()
+                )
+                .headers(headers -> headers.addAll(makeHeaders(keyPairId)))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<FutureAccountBalanceResponse>>() {
+                })
+                .block()
+        );
     }
 
     private HttpHeaders makeHeaders(String keyPairId) {
