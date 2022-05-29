f0a7d9d9058f28d05ca6a5f48f225855148b72ef
DingDongDeng <tpea999@gmail.com>
2022-05-29 17:43:42 +0900

    메소드명 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/client/BinanceFutureClient.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/client/BinanceFutureClient.java
@@ -4,10 +4,10 @@ import com.dingdongdeng.coinautotrading.common.client.ResponseHandler;
 import com.dingdongdeng.coinautotrading.common.client.util.QueryParamsConverter;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FutureChangeLeverageRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FutureChangePositionModeRequest;
+import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FutureNewOrderRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FutureOrderCancelRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FutureOrderInfoRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FuturesAccountBalanceRequest;
-import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureRequest.FutureNewOrderRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.BinanceServerTimeResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.FutureAccountBalanceResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.FutureChangeLeverageResponse;
@@ -15,8 +15,6 @@ import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFut
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.FutureNewOrderResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.FutureOrderCancelResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.client.model.BinanceFutureResponse.FutureOrderInfoResponse;
-import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitRequest.OrderCancelRequest;
-import com.dingdongdeng.coinautotrading.trading.exchange.client.model.UpbitResponse.OrderCancelResponse;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -84,7 +82,7 @@ public class BinanceFutureClient {
                 .uri("/fapi/v1/leverage")
                 .headers(headers -> headers.addAll(makeHeaders(keyPairId)))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .bodyValue(makeQueryParamForPOST(request, keyPairId))
+                .bodyValue(makeQueryParam(request, keyPairId))
                 .retrieve()
                 .bodyToMono(FutureChangeLeverageResponse.class)
                 .block()
@@ -98,7 +96,7 @@ public class BinanceFutureClient {
                 .uri("/fapi/v1/positionSide/dual")
                 .headers(headers -> headers.addAll(makeHeaders(keyPairId)))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .bodyValue(makeQueryParamForPOST(request, keyPairId))
+                .bodyValue(makeQueryParam(request, keyPairId))
                 .retrieve()
                 .bodyToMono(FutureChangePositionModeResponse.class)
                 .block()
@@ -111,7 +109,7 @@ public class BinanceFutureClient {
                 .uri("/fapi/v1/order")
                 .headers(headers -> headers.addAll(makeHeaders(keyPairId)))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .bodyValue(makeQueryParamForPOST(request, keyPairId))
+                .bodyValue(makeQueryParam(request, keyPairId))
                 .retrieve()
                 .bodyToMono(FutureNewOrderResponse.class)
                 .block()
@@ -143,7 +141,7 @@ public class BinanceFutureClient {
         return signatureWrapper.getSignatureRequest(request, token);
     }
 
-    private String makeQueryParamForPOST(Object request, String keyPairId){
+    private String makeQueryParam(Object request, String keyPairId) {
         return queryParamsConverter.convertStr(makeSignatureWrapper(request, keyPairId)).substring(1);
     }
 
