7773d7a4276e1f969b10ee8a824533a8280d2615
choozx <90917457+choozx@users.noreply.github.com>
2022-05-30 11:25:03 +0900

    model 복사

---
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/future/service/model/FutureExchangeOrderInfoParam.java
@@ -0,0 +1,13 @@
+package com.dingdongdeng.coinautotrading.trading.exchange.future.service.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Builder
+public class FutureExchangeOrderInfoParam {
+
+    private String orderId;
+}
