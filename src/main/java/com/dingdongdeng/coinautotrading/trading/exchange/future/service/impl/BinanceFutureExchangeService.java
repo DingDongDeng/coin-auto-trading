b1fd3f0b29f468671486ea3e095beccdd306861a
choozx <choozx23@gmail.com>
2022-06-01 17:06:44 +0900

    getTradingInfo 내용 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/future/service/impl/BinanceFutureExchangeService.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/exchange/future/service/impl/BinanceFutureExchangeService.java
@@ -2,16 +2,17 @@ package com.dingdongdeng.coinautotrading.trading.exchange.future.service.impl;
 
 import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
 import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
-import com.dingdongdeng.coinautotrading.trading.common.context.TradingTimeContext;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.BinanceFutureClient;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureEnum.Interval;
-import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureEnum.Side;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureEnum.Symbol;
+import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureRequest.FutureAccountBalanceRequest;
+import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureRequest.FutureMarkPriceRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureRequest.FutureCandleRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureRequest.FutureNewOrderRequest;
-import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureEnum.Type;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureRequest.FutureOrderCancelRequest;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureRequest.FutureOrderInfoRequest;
+import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureResponse.FutureAccountBalanceResponse;
+import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureResponse.FutureMarkPriceResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureResponse.FutureCandleResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureResponse.FutureNewOrderResponse;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.client.model.BinanceFutureResponse.FutureOrderCancelResponse;
@@ -26,11 +27,14 @@ import com.dingdongdeng.coinautotrading.trading.exchange.future.service.model.Fu
 import com.dingdongdeng.coinautotrading.trading.exchange.future.service.model.FutureExchangeTicker;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.service.model.FutureExchangeTradingInfo;
 import com.dingdongdeng.coinautotrading.trading.exchange.future.service.model.FutureExchangeTradingInfoParam;
-import com.dingdongdeng.coinautotrading.trading.exchange.spot.client.model.UpbitEnum.MarketType;
 import com.dingdongdeng.coinautotrading.trading.index.IndexCalculator;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -71,21 +75,27 @@ public class BinanceFutureExchangeService implements FutureExchangeService {
     public FutureExchangeTradingInfo getTradingInformation(FutureExchangeTradingInfoParam param, String keyPairId) {
         log.info("binance process : get trading information param = {}", param);
 
+
         // 캔들 정보 조회
-        FutureExchangeCandles candles = getExchangeCandles(param, keyPairId);
+        FutureExchangeCandles candles = getExchangeCandles(param);
 
         // 현재가 정보 조회
-        FutureExchangeTicker ticker = getExchangeTicker(param, keyPairId);
+        FutureExchangeTicker ticker = getExchangeTicker(param);
 
         // 계좌 정보 조회
         // fixme 선물이라면 현재 내가 들고 있는 포지션?
-        FutureOrderInfoResponse accounts = binanceFutureClient.getFutureOrderInfo(
-            FutureOrderInfoRequest.builder()
-                .build()
+        List<FutureAccountBalanceResponse> accounts = binanceFutureClient.getFuturesAccountBalance(
+            FutureAccountBalanceRequest.builder()
+                    .timestamp(System.currentTimeMillis())
+                    .build()
             ,keyPairId
         );
 
         return FutureExchangeTradingInfo.builder()
+                .coinType(param.getCoinType())
+                .coinExchangeType(getCoinExchangeType())
+                .tradingTerm(param.getTradingTerm())
+                .currency()
             .build();
     }
 
@@ -103,7 +113,7 @@ public class BinanceFutureExchangeService implements FutureExchangeService {
         return CoinExchangeType.BINANCE_FUTURE;
     }
 
-    private FutureExchangeCandles getExchangeCandles(FutureExchangeTradingInfoParam param, String keyPairId) {
+    private FutureExchangeCandles getExchangeCandles(FutureExchangeTradingInfoParam param) {
         TradingTerm tradingTerm = param.getTradingTerm();
         List<FutureCandleResponse> response = binanceFutureClient.getMinuteCandle(
             FutureCandleRequest.builder()
@@ -119,8 +129,8 @@ public class BinanceFutureExchangeService implements FutureExchangeService {
             .candleList(
                 response.stream().map(
                     candle -> FutureExchangeCandles.Candle.builder()
-                        .candleDateTimeUtc(candle.getCandleDateTimeUtc())
-                        .candleDateTimeKst(candle.getCandleDateTimeKst())
+                        .candleDateTimeUtc(convertTime(candle.getCloseTime()))
+                        .candleDateTimeKst(convertTime(candle.getCloseTime()))
                         .openingPrice(candle.getOpen())
                         .highPrice(candle.getHigh())
                         .lowPrice(candle.getLow())
@@ -144,14 +154,24 @@ public class BinanceFutureExchangeService implements FutureExchangeService {
             .build();
     }
 
-    /*private FutureExchangeTicker getExchangeTicker(FutureExchangeTradingInfoParam param, String keyPairId) {
-        TickerResponse response = upbitClient.getTicker(    //fixme 업비트의에서 ticker는 바이낸스의 https://binance-docs.github.io/apidocs/futures/en/#mark-price 이걸 참고하면 될듯
-            TickerRequest.builder()
-                .marketList(List.of(MarketType.of(param.getCoinType()).getCode()))
+    private FutureExchangeTicker getExchangeTicker(FutureExchangeTradingInfoParam param) {
+         FutureMarkPriceResponse response = binanceFutureClient.getMarkPrice(
+            FutureMarkPriceRequest.builder()
+                .symbol(Symbol.of(param.getCoinType()).getCode())
                 .build()
-            , keyPairId
-        ).stream().findFirst().orElseThrow(NoSuchElementException::new);
+        );
         return FutureExchangeTicker.builder()
-            .build();
-    }*/
+                .symbol(response.getSymbol())
+                .markPrice(response.getMarkPrice())
+                .indexPrice(response.getIndexPrice())
+                .estimatedSettlePrice(response.getEstimatedSettlePrice())
+                .lastFundingRate(response.getLastFundingRate())
+                .interestRate(response.getInterestRate())
+                .nextFundingTime(response.getNextFundingTime())
+                .build();
+    }
+
+    private LocalDateTime convertTime(Long timestamp){
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), TimeZone.getDefault().toZoneId());
+    }
 }
