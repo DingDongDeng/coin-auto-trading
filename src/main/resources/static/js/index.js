b5060b2aba3c01d75f8898d711dc7f68595e4552
DingDongDeng <tpea999@gmail.com>
2022-08-15 03:20:47 +0900

    pinia 설정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,16 @@
 import api from "./api.js"
 import dashboard from "./component/dashboard/dashboard.js"
 
-// use : {vuex}
+// global variable
+Vue.prototype.api = api.axios;
+
+// pinia : https://pinia.vuejs.org/getting-started.html#installation
+Vue.use(Pinia.PiniaVuePlugin)
+
 new Vue({
   el: '#app',
   vuetify: new Vuetify(),
+  pinia: Pinia.createPinia(),
   components: {
     dashboard
   },
@@ -12,8 +18,3 @@ new Vue({
   },
   methods: {},
 });
-
-// global variable
-Vue.prototype.api = api.axios;
-
-
