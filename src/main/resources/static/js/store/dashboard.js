064ca95a53ac3464bd1fb746fb9497ea6b942c88
DingDongDeng <tpea999@gmail.com>
2022-08-15 19:08:01 +0900

    store/dashboard.js 생성

---
+++ src/main/resources/static/js/store/dashboard.js
@@ -0,0 +1,67 @@
+import Pinia from "../lib/pinia.js";
+import Axios from "../lib/axios.js";
+
+const api = Axios.create({
+  timeout: 5000,
+});
+api.interceptors.response.use((response) => response, (error) => {
+  alert(error);
+  throw error;
+});
+
+async function getUserKeyList() {
+  const response = await api.get("/user/key/pair");
+  return response.data.body;
+}
+
+async function getUserAutoTradingList() {
+  const response = await api.get("/user/autotrading");
+  return response.data.body;
+}
+
+async function getUserBackTestingList() {
+  const response = await api.get("/user/backtesting");
+  return response.data.body;
+}
+
+async function getTypeInfo() {
+  const response = await api.get("/type");
+  return response.data.body;
+}
+
+async function getStrategyMeta(strategyCode) {
+  if (strategyCode) {
+    const response = await api.get("/" + strategyCode + "/meta");
+    return response.data.body.paramMetaList;
+  }
+  return [];
+}
+
+export const useDashboardStore = Pinia.defineStore('dashboard', {
+  state: () => ({
+    user: {
+      keyPairList: [],
+      autoTradingList: [],
+      backTestingList: []
+    },
+    register: {
+      keyPair: {},
+      autoTrading: {},
+      backTesting: {}
+    },
+    type: {}
+  }),
+  actions: {
+    async refresh() {
+      console.log("test refresh")
+      this.user.keyPairList = await getUserKeyList();
+      this.user.autoTradingList = await getUserAutoTradingList();
+      this.user.backTestingList = await getUserBackTestingList();
+      this.type = await getTypeInfo();
+      this.register.autoTrading.strategyCoreParamMetaList = await getStrategyMeta(
+          this.register.autoTrading.strategyCode
+      );
+    }
+  },
+  getters: {}
+})
