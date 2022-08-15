143ff66ddcf4c0c8b38889af3062bbda1198ec80
DingDongDeng <tpea999@gmail.com>
2022-08-15 19:12:33 +0900

    index.js 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,15 +1,7 @@
 import Vue from "./lib/vue.js"
 import Pinia from "./lib/pinia.js"
-import Axios from "./lib/axios.js"
+import axios from "./lib/axios.js"
 import dashboard from "./component/dashboard/dashboard.js"
-
-const axios = Axios.create({
-  timeout: 5000,
-});
-axios.interceptors.response.use((response) => response, (error) => {
-  alert(error);
-  throw error;
-});
 
 Vue.prototype.api = axios;
 
