7632d1c727e7f2e2ffc6823e661c5c95e5052c78
DingDongDeng <tpea999@gmail.com>
2022-08-15 19:15:12 +0900

    dashboard.js에 dashboard-gnb 컴포넌트 추가

--- src/main/resources/static/js/component/dashboard/dashboard.js
+++ src/main/resources/static/js/component/dashboard/dashboard.js
@@ -1,27 +1,10 @@
+import dashboardGnb from "./dashboard-gnb.js"
+
 export default Vue.component('dashboard', {
   template: `
 <v-container>
   <!-- 헤더 -->
-  <v-container>
-    <v-toolbar dark color="primary" style="height:64px;">
-      <v-toolbar-title class="white--text"></v-toolbar-title>
-
-      <v-toolbar-items>
-        <v-icon @click="refresh()">cached</v-icon>
-      </v-toolbar-items>
-
-      <v-spacer></v-spacer>
-
-      <v-form action="/logout" method="post">
-        <v-btn
-            type="submit"
-            text
-        >Logout
-        </v-btn>
-      </v-form>
-    </v-toolbar>
-  </v-container>
-
+  <dashboard-gnb/>
   <!-- 모달 -->
   <v-dialog
       v-model="register.backTesting.isVisible"
@@ -295,7 +278,9 @@ export default Vue.component('dashboard', {
   </v-container>
 </v-container>
 `,
-  components: {},
+  components: {
+    dashboardGnb
+  },
   data() {
     return {
       user: {
