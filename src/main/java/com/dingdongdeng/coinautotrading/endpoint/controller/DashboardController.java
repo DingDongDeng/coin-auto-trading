719a498a5625255fdad2e229a352aec0aec11359
DingDongDeng <tpea999@gmail.com>
2022-08-21 02:10:27 +0900

    ConsoleController.java -> DashboardController 로 이름 바꾸고 패키지 이동

--- src/main/java/com/dingdongdeng/coinautotrading/endpoint/controller/DashboardController.java
+++ src/main/java/com/dingdongdeng/coinautotrading/endpoint/controller/DashboardController.java
@@ -1,19 +1,26 @@
-package com.dingdongdeng.coinautotrading.console.controller;
+package com.dingdongdeng.coinautotrading.endpoint.controller;
 
 import com.dingdongdeng.coinautotrading.common.model.CommonResponse;
 import com.dingdongdeng.coinautotrading.common.type.CoinExchangeType;
 import com.dingdongdeng.coinautotrading.common.type.CoinType;
 import com.dingdongdeng.coinautotrading.common.type.TradingTerm;
-import com.dingdongdeng.coinautotrading.console.controller.model.TypeInfoResponse;
-import com.dingdongdeng.coinautotrading.console.controller.model.TypeInfoResponse.TypeInfoTemplate;
+import com.dingdongdeng.coinautotrading.endpoint.controller.DashboardController.TypeInfoResponse.TypeInfoTemplate;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.type.StrategyCode;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class ConsoleController {
+public class DashboardController {
 
     @RequestMapping("/")
     public String index() {
@@ -37,4 +44,33 @@ public class ConsoleController {
     }
 
 
+    @ToString
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class TypeInfoResponse {
+
+        private List<TypeInfoTemplate> coinTypeList;
+        private List<TypeInfoTemplate> coinExchangeTypeList;
+        private List<TypeInfoTemplate> tradingTermList;
+        private List<TypeInfoTemplate> strategyCodeList;
+
+        @ToString
+        @Getter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Builder
+        public static class TypeInfoTemplate {
+
+            private String name;
+            private Object value;
+
+            public static List<TypeInfoTemplate> listOf(Map<?, String> enumMap) {
+                return enumMap.entrySet().stream()
+                    .map(entry -> new TypeInfoTemplate(entry.getValue(), entry.getKey()))
+                    .collect(Collectors.toList());
+            }
+        }
+    }
 }
