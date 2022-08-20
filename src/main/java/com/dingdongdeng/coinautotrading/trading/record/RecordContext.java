77bdd7fd854a80c0f6e9ae8c7590a19679ab7e8e
DingDongDeng <tpea999@gmail.com>
2022-08-21 01:01:23 +0900

    RecordContext 생성

---
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/record/RecordContext.java
@@ -0,0 +1,28 @@
+package com.dingdongdeng.coinautotrading.trading.record;
+
+import com.dingdongdeng.coinautotrading.trading.exchange.common.model.ExchangeCandles.Candle;
+import com.dingdongdeng.coinautotrading.trading.index.Index;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.StrategyExecuteResult;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingInfo;
+import com.dingdongdeng.coinautotrading.trading.strategy.model.TradingResult;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RecordContext {
+
+    private Candle currentCandle;
+    private Index index;
+    private List<TradingResult> tradingResultList;
+
+    public static RecordContext ofStrategyExecuteResult(StrategyExecuteResult executeResult) {
+        TradingInfo tradingInfo = executeResult.getTradingInfo();
+        return new RecordContext(
+            tradingInfo.getCandles().getLatest(0),
+            tradingInfo.getIndex(),
+            executeResult.getTradingResultList()
+        );
+    }
+}
