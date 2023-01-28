982a191ebcfb9f4b44226172fb5e4ee00461d62a
DingDongDeng <tpea999@gmail.com>
2023-01-28 20:55:18 +0900

    설명 수정

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ResistanceTradingStrategyCoreParam.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/ResistanceTradingStrategyCoreParam.java
@@ -18,7 +18,7 @@ public class ResistanceTradingStrategyCoreParam implements StrategyCoreParam {
     @GuideMessage("최초 주문할 금액을 입력해주세요. ex) 40000")
     private double initOrderPrice; // 처음에 주문할 금액
 
-    @GuideMessage("저항선 버퍼 금액(n원을 설정하면 저항/지지선 기준으로 n원의 버퍼를 둠) ex) 8000")
+    @GuideMessage("저항선 버퍼 금액(n원을 설정하면 저항/지지선 기준으로 n원의 버퍼를 둠) ex) 3000")
     private double resistancePriceBuffer;
 
     @GuideMessage("계좌 안전 금액을 입력해주세요.")
