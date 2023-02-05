4ddfd217e78f826146ff5a14e725dcbf8585dbb1
DingDongDeng <tpea999@gmail.com>
2023-02-05 17:43:47 +0900

    중간 커밋

--- src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/TrendSwitchTradingStrategyCore.java
+++ src/main/java/com/dingdongdeng/coinautotrading/trading/strategy/core/TrendSwitchTradingStrategyCore.java
@@ -1,5 +1,7 @@
 package com.dingdongdeng.coinautotrading.trading.strategy.core;
 
+import com.dingdongdeng.coinautotrading.trading.exchange.common.model.ExchangeCandles;
+import com.dingdongdeng.coinautotrading.trading.exchange.common.model.ExchangeCandles.Candle;
 import com.dingdongdeng.coinautotrading.trading.index.Index;
 import com.dingdongdeng.coinautotrading.trading.strategy.StrategyCore;
 import com.dingdongdeng.coinautotrading.trading.strategy.model.SpotTradingInfo;
@@ -18,7 +20,7 @@ public class TrendSwitchTradingStrategyCore implements StrategyCore<SpotTradingI
     private final TrendSwitchTradingStrategyCoreParam param;
 
     private final BBandsTradingStrategyCore bBandsTradingStrategyCore;
-    private final ResistanceTradingStrategyCore resistanceTradingStrategyCore;
+    private final MacdTradingStrategyCore macdTradingStrategyCore;
 
     public TrendSwitchTradingStrategyCore(TrendSwitchTradingStrategyCoreParam param) {
         this.param = param;
@@ -31,10 +33,10 @@ public class TrendSwitchTradingStrategyCore implements StrategyCore<SpotTradingI
                 .processDuration(param.getProcessDuration())
                 .build()
         );
-        this.resistanceTradingStrategyCore = new ResistanceTradingStrategyCore(
-            ResistanceTradingStrategyCoreParam.builder()
+        this.macdTradingStrategyCore = new MacdTradingStrategyCore(
+            MacdTradingStrategyCoreParam.builder()
                 .initOrderPrice(param.getInitOrderPrice())
-                .resistancePriceBuffer(param.getConditionTimeBuffer())
+                .conditionTimeBuffer(param.getConditionTimeBuffer())
                 .accountBalanceLimit(param.getAccountBalanceLimit())
                 .tooOldOrderTimeSeconds(param.getTooOldOrderTimeSeconds())
                 .processDuration(param.getProcessDuration())
@@ -44,37 +46,80 @@ public class TrendSwitchTradingStrategyCore implements StrategyCore<SpotTradingI
 
     @Override
     public List<TradingTask> makeTradingTask(SpotTradingInfo tradingInfo, TradingResultPack<SpotTradingResult> tradingResultPack) {
-        double currentPrice = tradingInfo.getCurrentPrice();
         Index index = tradingInfo.getIndex();
-        double sma200 = index.getMa().getSma200();
+        ExchangeCandles candles = tradingInfo.getCandles();
+
+        /**
+         * FIXME
+         *  볼린저전략 손절 미끄럼틀 해결 필요
+         *      - macd를 볼까??
+         *
+         *  TODO
+         *      하락장은 lower에 사서 middel 판매
+         *      횡보장은 lower에 사서 higer에 판매
+         */
 
         // 상승장이라면
-        if (sma200 < currentPrice) {
+        if (isUptrend(index, candles)) {
             log.info("[분기 조건] 상승 추세");
-            return resistanceTradingStrategyCore.makeTradingTask(tradingInfo, tradingResultPack);
-        } else if (false) { //FIXME 추세 구분 고도화 필요
-            log.info("[분기 조건] 횡보 추세");
-            return bBandsTradingStrategyCore.makeTradingTask(tradingInfo, tradingResultPack);
-        } else {
+            return macdTradingStrategyCore.makeTradingTask(tradingInfo, tradingResultPack);
+        }
+        // 하락장이라면
+        else if (isDowntrend(index, candles)) {
             log.info("[분기 조건] 하락 추세");
+            return bBandsTradingStrategyCore.makeTradingTask(tradingInfo, tradingResultPack);
+        }
+        // 횡보장이라면
+        else {
+            log.info("[분기 조건] 횡보 추세");
             return bBandsTradingStrategyCore.makeTradingTask(tradingInfo, tradingResultPack);
         }
     }
 
     @Override
     public void handleOrderResult(SpotTradingResult tradingResult) {
-        resistanceTradingStrategyCore.handleOrderResult(tradingResult);
+        macdTradingStrategyCore.handleOrderResult(tradingResult);
         bBandsTradingStrategyCore.handleOrderResult(tradingResult);
     }
 
     @Override
     public void handleOrderCancelResult(SpotTradingResult tradingResult) {
-        resistanceTradingStrategyCore.handleOrderCancelResult(tradingResult);
+        macdTradingStrategyCore.handleOrderCancelResult(tradingResult);
         bBandsTradingStrategyCore.handleOrderCancelResult(tradingResult);
     }
 
     @Override
     public StrategyCoreParam getParam() {
         return this.param;
+    }
+
+    private boolean isUptrend(Index index, ExchangeCandles candles) {
+        int TARGET_CANDLE_COUNT = 10;
+        double ema60 = index.getMa().getEma60();
+        List<Candle> candleList = candles.getCandleList();
+        int offset = candleList.size() - TARGET_CANDLE_COUNT;
+
+        for (int i = offset; i < candleList.size(); i++) {
+            if (candleList.get(i).getTradePrice() < ema60) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    private boolean isDowntrend(Index index, ExchangeCandles candles) {
+        int TARGET_CANDLE_COUNT = 10;
+        double ema60 = index.getMa().getEma60();
+        List<Candle> candleList = candles.getCandleList();
+        int offset = candleList.size() - TARGET_CANDLE_COUNT;
+
+        for (int i = offset; i < candleList.size(); i++) {
+            if (candleList.get(i).getTradePrice() > ema60) {
+                return false;
+            }
+
+        }
+        return true;
     }
 }
