151c197b4ec5c87f16d3335aabb1d7297e9a13b7
DingDongDeng <tpea999@gmail.com>
2023-11-18 16:18:41 +0900

    OrderState 코틀린으로 변경

---
+++ infrastructure/common/src/main/kotlin/com/dingdongdeng/autotrading/type/OrderState.kt
@@ -0,0 +1,11 @@
+package com.dingdongdeng.autotrading.type
+
+enum class OrderState(
+    val desc: String
+) {
+    WAIT("체결 대기"),
+    WATCH("예약 주문 대기"),
+    DONE("체결 완료"),
+    CANCEL("주문 취소"),
+    LIQUIDATION("주문 청산");
+}
