8b6981d19ce49a1a39ab206596669cac54b61e87
DingDongDeng <tpea999@gmail.com>
2023-11-18 15:16:12 +0900

    새 모듈들의 build.gradle 생성

---
+++ coin-futures/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("project.application-conventions")
+    id("project.integration-test-conventions")
+    id("project.kotlin-conventions")
+    id("project.spring-conventions")
+}
+
+dependencies {
+}
---
+++ coin-spot/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("project.application-conventions")
+    id("project.integration-test-conventions")
+    id("project.kotlin-conventions")
+    id("project.spring-conventions")
+}
+
+dependencies {
+
+}
