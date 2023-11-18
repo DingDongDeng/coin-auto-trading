846047eb3b97fa4be094b1f9d7964e3dacc05db1
DingDongDeng <tpea999@gmail.com>
2023-11-18 15:55:04 +0900

    infrastructur.client 모듈 생성

---
+++ infrastructure/client/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("project.kotlin-conventions")
+    id("project.module-conventions")
+    id("project.spring-conventions")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
