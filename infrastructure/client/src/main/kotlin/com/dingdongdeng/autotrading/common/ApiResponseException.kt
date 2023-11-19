f84b4707898c6d6df912aa211adf626ef7bcd75f
DingDongDeng <tpea999@gmail.com>
2023-11-19 16:01:36 +0900

    ss

--- infrastructure/client/src/main/kotlin/com/dingdongdeng/autotrading/common/ApiResponseException.kt
+++ infrastructure/client/src/main/kotlin/com/dingdongdeng/autotrading/common/ApiResponseException.kt
@@ -2,7 +2,7 @@ package com.dingdongdeng.autotrading.common
 
 import org.springframework.http.HttpStatus
 
-data class ApiResponseException(
+class ApiResponseException(
     val status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
     val body: String = "",
     override val cause: Throwable,
