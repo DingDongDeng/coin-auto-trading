d8f63498d77b39e8b7c2d817df32c5874dca7240
DingDongDeng <tpea999@gmail.com>
2023-12-26 19:04:22 +0900

    client모듈에 upbit 토큰을 위한 암호 의존성 추가

--- infrastructure/client/build.gradle.kts
+++ infrastructure/client/build.gradle.kts
@@ -8,4 +8,5 @@ dependencies {
     implementation(project(":infrastructure:common"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.auth0:java-jwt:3.18.2") // upbit api 헤더 생성을 위함
 }
