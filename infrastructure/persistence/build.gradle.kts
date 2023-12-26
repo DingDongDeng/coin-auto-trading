da8699f5620dc87923141e7e0c54d9e3e75fd66e
DingDongDeng <tpea999@gmail.com>
2023-12-26 19:04:06 +0900

    persistence 모듈에 JPA, querydsl 의존성 추가

--- infrastructure/persistence/build.gradle.kts
+++ infrastructure/persistence/build.gradle.kts
@@ -5,5 +5,12 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":infrastructure:common"))
+    // jpa
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
+    runtimeOnly("com.h2database:h2")
 }
