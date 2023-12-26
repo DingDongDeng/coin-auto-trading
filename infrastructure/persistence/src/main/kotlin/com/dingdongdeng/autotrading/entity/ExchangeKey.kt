69c70b913d8845816d2a948a683304e707914367
DingDongDeng <tpea999@gmail.com>
2023-12-26 19:05:07 +0900

    persistence모듈에 ExchangeKey 엔티티 생성

---
+++ infrastructure/persistence/src/main/kotlin/com/dingdongdeng/autotrading/entity/ExchangeKey.kt
@@ -0,0 +1,31 @@
+package com.dingdongdeng.autotrading.entity
+
+import com.dingdongdeng.autotrading.type.CoinExchangeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "exchange_key")
+class ExchangeKey(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exchange_key_id")
+    val id: Long? = null,
+    @Column(name = "pair_id")
+    val pairId: String,
+    @Enumerated(EnumType.STRING)
+    @Column(name = "coin_exchange_type")
+    val coinExchangeType: CoinExchangeType,
+    @Column(name = "name")
+    val name: String,
+    @Column(name = "`value`")
+    val value: String,
+    @Column(name = "user_id")
+    val userId: String,
+)
