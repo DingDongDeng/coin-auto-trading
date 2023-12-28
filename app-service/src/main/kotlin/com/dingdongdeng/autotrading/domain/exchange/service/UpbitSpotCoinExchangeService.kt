4b67e681c8a330a7df7fbf63648851b66406d4e0
DingDongDeng <tpea999@gmail.com>
2023-12-29 01:16:31 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/service/UpbitSpotCoinExchangeService.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/service/UpbitSpotCoinExchangeService.kt
@@ -2,10 +2,12 @@ package com.dingdongdeng.autotrading.domain.exchange.service
 
 import com.dingdongdeng.autotrading.domain.exchange.SpotCoinExchangeService
 import com.dingdongdeng.autotrading.domain.exchange.model.ExchangeKeyParam
+import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeCandleResult
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeChartParam
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeChartResult
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeOrderParam
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeOrderResult
+import com.dingdongdeng.autotrading.infra.client.upbit.CandleRequest
 import com.dingdongdeng.autotrading.infra.client.upbit.MarketType
 import com.dingdongdeng.autotrading.infra.client.upbit.OrdType
 import com.dingdongdeng.autotrading.infra.client.upbit.OrderCancelRequest
@@ -14,6 +16,7 @@ import com.dingdongdeng.autotrading.infra.client.upbit.OrderRequest
 import com.dingdongdeng.autotrading.infra.client.upbit.Side
 import com.dingdongdeng.autotrading.infra.client.upbit.UpbitApiClient
 import com.dingdongdeng.autotrading.infra.client.upbit.UpbitTokenGenerator
+import com.dingdongdeng.autotrading.infra.common.log.Slf4j.Companion.log
 import com.dingdongdeng.autotrading.infra.common.type.CoinExchangeType
 import org.springframework.stereotype.Service
 
@@ -76,8 +79,66 @@ class UpbitSpotCoinExchangeService(
         )
     }
 
+    // from <= 조회범위 <= to
     override fun getChart(param: SpotCoinExchangeChartParam, keyParam: ExchangeKeyParam): SpotCoinExchangeChartResult {
-        TODO("Not yet implemented")
+        val request = CandleRequest(
+            unit = param.candleUnit.size,
+            market = MarketType.of(param.coinType).code,
+            timeAsKst = param.to,
+            count = param.chunkSize,
+        )
+        var response = upbitApiClient
+            .getCandle(request, param.candleUnit, makeToken(request, keyParam))
+            .sortedBy { it.candleDateTimeKst } // 가장 마지막 캔들이 현재 캔들
+
+        // from <= 조회 범위 <= to 를 만족하도록 부족한 캔들을 조회
+        while (true) {
+            val firstCandle = response.first()
+            val isCompleted = firstCandle.candleDateTimeKst.isBefore(param.from)
+            if (isCompleted) {
+                log.info("캔들 조회 완료, from={}, to={}", param.from, param.to)
+                break
+            }
+
+            val request2 = CandleRequest(
+                unit = param.candleUnit.size,
+                market = MarketType.of(param.coinType).code,
+                timeAsKst = firstCandle.candleDateTimeKst,
+                count = param.chunkSize,
+            )
+            val response2 = upbitApiClient
+                .getCandle(request2, param.candleUnit, makeToken(request2, keyParam))
+                .sortedBy { it.candleDateTimeKst } // 가장 마지막 캔들이 현재 캔들
+
+            response = response2 + response
+        }
+
+        // 범위를 넘는 캔들 커팅
+        response =
+            response.filter { (it.candleDateTimeKst.isAfter(param.to) || it.candleDateTimeKst.isBefore(param.from)).not() }
+
+        return SpotCoinExchangeChartResult(
+            coinExchangeType = EXCHANGE_TYPE,
+            coinType = param.coinType,
+            from = param.from,
+            to = param.to,
+            currentPrice = response.last().tradePrice,
+            candles = response.map {
+                SpotCoinExchangeCandleResult(
+                    coinExchangeType = EXCHANGE_TYPE,
+                    coinType = param.coinType,
+                    candleUnit = param.candleUnit,
+                    candleDateTimeUtc = it.candleDateTimeUtc,
+                    candleDateTimeKst = it.candleDateTimeKst,
+                    openingPrice = it.openingPrice,
+                    highPrice = it.highPrice,
+                    lowPrice = it.lowPrice,
+                    closingPrice = it.tradePrice,
+                    accTradePrice = it.candleAccTradePrice,
+                    accTradeVolume = it.candleAccTradeVolume,
+                )
+            }
+        )
     }
 
     override fun support(exchangeType: CoinExchangeType): Boolean {
