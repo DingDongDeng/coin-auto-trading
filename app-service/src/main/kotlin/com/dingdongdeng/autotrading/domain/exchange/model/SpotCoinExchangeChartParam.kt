dca80a693312a88d1fa48fddf3bd22104959134d
DingDongDeng <tpea999@gmail.com>
2023-12-29 01:16:26 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/model/SpotCoinExchangeChartParam.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/model/SpotCoinExchangeChartParam.kt
@@ -9,4 +9,5 @@ data class SpotCoinExchangeChartParam(
     val candleUnit: CandleUnit,
     val from: LocalDateTime,
     val to: LocalDateTime,
+    val chunkSize: Int = 200, // 한번에 조회하는 개수 (1000개를 조회하더라도 200개씩 나눠서 조회)
 )
