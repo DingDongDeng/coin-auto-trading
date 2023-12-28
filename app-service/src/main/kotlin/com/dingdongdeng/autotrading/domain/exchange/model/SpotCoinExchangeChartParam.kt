49e2c66aa6222f7858ce469d2fb1672ad50cee8e
DingDongDeng <tpea999@gmail.com>
2023-12-29 00:39:07 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/model/SpotCoinExchangeChartParam.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/model/SpotCoinExchangeChartParam.kt
@@ -1,10 +1,12 @@
 package com.dingdongdeng.autotrading.domain.exchange.model
 
+import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import java.time.LocalDateTime
 
 data class SpotCoinExchangeChartParam(
     val coinType: CoinType,
+    val candleUnit: CandleUnit,
     val from: LocalDateTime,
     val to: LocalDateTime,
 )
