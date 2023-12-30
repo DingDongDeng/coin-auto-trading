32c00ed61fd9b50b03d1b66452e1e0c1360e41e6
DingDongDeng <tpea999@gmail.com>
2023-12-31 00:32:37 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/type/CoinStrategyType.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/type/CoinStrategyType.kt
@@ -2,7 +2,7 @@ package com.dingdongdeng.autotrading.domain.strategy.type
 
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
 
-enum class StrategyType(
+enum class CoinStrategyType(
     val desc: String,
     val exchangeType: ExchangeType
 ) {
