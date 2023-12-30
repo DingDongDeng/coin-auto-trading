aed3edb39adca3df6d6941a78808d8401867e07c
DingDongDeng <tpea999@gmail.com>
2023-12-31 00:31:39 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/TestSpotCoinStrategy.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/TestSpotCoinStrategy.kt
@@ -2,17 +2,18 @@ package com.dingdongdeng.autotrading.domain.strategy.service
 
 import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyMakeTaskParam
 import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyMakeTaskResult
+import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
 
 class TestSpotCoinStrategy : SpotCoinStrategy {
     override fun makeTask(params: List<SpotCoinStrategyMakeTaskParam>): List<SpotCoinStrategyMakeTaskResult> {
         TODO("Not yet implemented")
     }
 
-    override fun handleTaskResult() {
+    override fun handleResult() {
         TODO("Not yet implemented")
     }
 
-    override fun getStrategyType() {
+    override fun support(param: CoinStrategyType): Boolean {
         TODO("Not yet implemented")
     }
 }
