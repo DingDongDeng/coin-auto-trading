63df6dba3a8d94b27db3bae3e293a2cbd8154302
DingDongDeng <tpea999@gmail.com>
2023-12-30 17:09:42 +0900

    작성중

---
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/autotrade/service/AutoTradeService.kt
@@ -0,0 +1,42 @@
+package com.dingdongdeng.autotrading.domain.autotrade.service
+
+import com.dingdongdeng.autotrading.domain.autotrade.model.AutoTradeProcessor
+import com.dingdongdeng.autotrading.infra.client.slack.SlackSender
+import org.springframework.stereotype.Service
+
+@Service
+class AutoTradeService(
+    private val slackSender: SlackSender,
+) {
+
+    private val storedProcessors = mutableMapOf<String, AutoTradeProcessor>()
+
+    fun get(autoTradeProcessorId: String): AutoTradeProcessor {
+        return storedProcessors[autoTradeProcessorId] ?: throw NoSuchElementException()
+    }
+
+    fun register(userId: Long, process: () -> Unit, duration: Long): String {
+        val processor = AutoTradeProcessor(
+            userId = userId,
+            process = process,
+            duration = duration,
+            slackSender = slackSender
+        )
+
+        storedProcessors[processor.id] = processor
+        return processor.id
+    }
+
+    fun start(autoTradeProcessorId: String) {
+        storedProcessors[autoTradeProcessorId]?.start()
+    }
+
+    fun stop(autoTradeProcessorId: String) {
+        storedProcessors[autoTradeProcessorId]?.stop()
+    }
+
+    fun terminate(autoTradeProcessorId: String) {
+        storedProcessors[autoTradeProcessorId]?.terminate()
+        storedProcessors.remove(autoTradeProcessorId)
+    }
+}
