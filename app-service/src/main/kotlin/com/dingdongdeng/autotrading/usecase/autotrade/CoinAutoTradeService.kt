5f52cf4c9879e0613012023c81903610a2dd1a6f
DingDongDeng <tpea999@gmail.com>
2023-12-31 14:45:22 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/usecase/autotrade/CoinAutoTradeService.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/usecase/autotrade/CoinAutoTradeService.kt
@@ -1,6 +1,7 @@
 package com.dingdongdeng.autotrading.usecase.autotrade
 
 import com.dingdongdeng.autotrading.domain.autotrade.service.AutoTradeService
+import com.dingdongdeng.autotrading.domain.exchange.model.ExchangeKeyPair
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeChartParam
 import com.dingdongdeng.autotrading.domain.exchange.service.SpotCoinExchangeService
 import com.dingdongdeng.autotrading.domain.indicator.service.IndicatorService
@@ -28,95 +29,116 @@ class CoinAutoTradeService(
     private val tradeHistoryService: TradeHistoryService,
 ) {
 
-    //FIXME 성능도 생각해야겠는데....? 백테스팅때 죽어나갈듯...  <<< strategy 안에서 병렬처리를 하던... 거기서 하는게 맞을듯
     fun register(
         userId: Long,
         coinStrategyType: CoinStrategyType,
         exchangeType: ExchangeType,
         coinTypes: List<CoinType>,
-        candleUnits: List<CandleUnit>, //TODO
+        candleUnits: List<CandleUnit>,
         keyPairId: String,
-    ) {
+        //TODO strategy에서 사용할 커스텀 파라미터도 추가 필요해
+    ): String {
+
         val autoTradeProcessorId = UUID.randomUUID().toString()
         val strategyService = strategyServices.first { it.support(coinStrategyType) }
         val exchangeService = exchangeServices.first { it.support(exchangeType) }
-        val keyParam = exchangeService.getExchangeKeyPair(keyPairId)
+        val exchangeKeyPair = exchangeService.getExchangeKeyPair(keyPairId)
 
         val process = {
             val makeTaskParams = coinTypes.map { coinType ->
                 SpotCoinStrategyMakeTaskParam(
                     exchangeType = exchangeType,
                     coinType = coinType,
-                    charts = candleUnits.map { candleUnit ->
-                        val now = TimeContext.now()
-                        val chartParam = SpotCoinExchangeChartParam(
-                            coinType = coinType,
-                            candleUnit = candleUnit,
-                            from = now.minusMinutes(2 * CHART_CANDLE_MAX_COUNT * candleUnit.getMinuteSize()), // 보조지표 계산을 위해 2배로 조회
-                            to = now,
-                        )
-
-                        val chart = exchangeService.getChart(chartParam, keyParam)
-
-                        val strategyChartCandleParams = mutableListOf<SpotCoinStrategyChartCandleParam>()
-                        var count = 0
-                        for ((index, candle) in chart.candles.withIndex().reversed()) {
-                            if (count >= CHART_CANDLE_MAX_COUNT) {
-                                break
-                            }
-                            val indicator = indicatorService.calculate(
-                                chart.candles.subList(
-                                    max(0, chart.candles.size - CHART_CANDLE_MAX_COUNT - index),
-                                    chart.candles.size - 1 - index
-                                )
-                            )
-                            if (candle.candleDateTimeKst.isEqual(indicator.indicatorDateTimeKst).not()) {
-                                throw RuntimeException("캔들의 시간과 보조지표의 시간이 다름 (예상한 계산 결과가 아님)")
-                            }
-
-                            strategyChartCandleParams.add(
-                                SpotCoinStrategyChartCandleParam(
-                                    candleUnit = candle.candleUnit,
-                                    candleDateTimeUtc = candle.candleDateTimeUtc,
-                                    candleDateTimeKst = candle.candleDateTimeKst,
-                                    openingPrice = candle.openingPrice,
-                                    highPrice = candle.highPrice,
-                                    lowPrice = candle.lowPrice,
-                                    closingPrice = candle.closingPrice,
-                                    accTradePrice = candle.accTradePrice,
-                                    accTradeVolume = candle.accTradeVolume,
-                                    indicators = indicator,
-                                )
-                            )
-                            count++
-                        }
-                        strategyChartCandleParams.sortBy { it.candleDateTimeKst }
-
-                        SpotCoinStrategyChartParam(
-                            from = chart.from,
-                            to = chart.to,
-                            currentPrice = chart.currentPrice,
-                            candleUnit = candleUnit,
-                            candles = strategyChartCandleParams,
-                        )
-                    },
-                    tradeInfo = SpotCoinStrategyTradeInfoParam(
-                        volume = 0,
-                        averagePrice = 0,
-                        lossProfitPrice = 0, // 실현 손익과, 평가 손익을 구분하고 싶어
-                        coinTradeHistory = tradeHistoryService.findAllTradeHistory(autoTradeProcessorId, coinType)
-                    )
+                    charts = makeSpotCoinStrategyChartParam(
+                        exchangeService = exchangeService,
+                        coinType = coinType,
+                        candleUnits = candleUnits,
+                        exchangeKeyPair = exchangeKeyPair,
+                    ),
+                    tradeInfo = makeSpotCoinStrategyTradeInfoParam(
+                        autoTradeProcessorId = autoTradeProcessorId,
+                        coinType = coinType,
+                    ),
                 )
             }
             strategyService.makeTask(makeTaskParams)
             strategyService.handleResult()
         }
 
-        val autoTradeProcessor = autoTradeService.register(autoTradeProcessorId, userId, process, 10000)
+        return autoTradeService.register(autoTradeProcessorId, userId, process, 10000)
     }
 
-    private fun makeProcess() {
+    private fun makeSpotCoinStrategyChartParam(
+        exchangeService: SpotCoinExchangeService,
+        coinType: CoinType,
+        candleUnits: List<CandleUnit>,
+        exchangeKeyPair: ExchangeKeyPair
+    ): List<SpotCoinStrategyChartParam> {
+        return candleUnits.map { candleUnit ->
+            val now = TimeContext.now()
+            val chartParam = SpotCoinExchangeChartParam(
+                coinType = coinType,
+                candleUnit = candleUnit,
+                from = now.minusMinutes(2 * CHART_CANDLE_MAX_COUNT * candleUnit.getMinuteSize()), // 보조지표 계산을 위해 2배로 조회
+                to = now,
+            )
 
+            val chart = exchangeService.getChart(chartParam, exchangeKeyPair)
+
+            val strategyChartCandleParams = mutableListOf<SpotCoinStrategyChartCandleParam>()
+            var count = 0
+            for ((index, candle) in chart.candles.withIndex().reversed()) {
+                if (count >= CHART_CANDLE_MAX_COUNT) {
+                    break
+                }
+                val indicator = indicatorService.calculate(
+                    chart.candles.subList(
+                        max(0, chart.candles.size - CHART_CANDLE_MAX_COUNT - index),
+                        chart.candles.size - 1 - index
+                    )
+                )
+                if (candle.candleDateTimeKst.isEqual(indicator.indicatorDateTimeKst).not()) {
+                    throw RuntimeException("캔들의 시간과 보조지표의 시간이 다름 (예상한 계산 결과가 아님)")
+                }
+
+                strategyChartCandleParams.add(
+                    SpotCoinStrategyChartCandleParam(
+                        candleUnit = candle.candleUnit,
+                        candleDateTimeUtc = candle.candleDateTimeUtc,
+                        candleDateTimeKst = candle.candleDateTimeKst,
+                        openingPrice = candle.openingPrice,
+                        highPrice = candle.highPrice,
+                        lowPrice = candle.lowPrice,
+                        closingPrice = candle.closingPrice,
+                        accTradePrice = candle.accTradePrice,
+                        accTradeVolume = candle.accTradeVolume,
+                        indicators = indicator,
+                    )
+                )
+                count++
+            }
+            strategyChartCandleParams.sortBy { it.candleDateTimeKst }
+
+            SpotCoinStrategyChartParam(
+                from = chart.from,
+                to = chart.to,
+                currentPrice = chart.currentPrice,
+                candleUnit = candleUnit,
+                candles = strategyChartCandleParams,
+            )
+        }
+    }
+
+    private fun makeSpotCoinStrategyTradeInfoParam(
+        autoTradeProcessorId: String,
+        coinType: CoinType,
+    ): SpotCoinStrategyTradeInfoParam {
+        return SpotCoinStrategyTradeInfoParam(
+            volume = 0,
+            averagePrice = 0,
+            lossProfitPrice = 0, //TODO 실현 손익과, 평가 손익을 구분하고 싶어
+            coinTradeHistory = tradeHistoryService.findAllTradeHistory(autoTradeProcessorId, coinType)
+        )
     }
 
     companion object {
