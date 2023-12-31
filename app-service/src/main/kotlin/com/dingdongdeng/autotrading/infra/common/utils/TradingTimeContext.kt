367c1270f111646cb9738b8368a7f57a9e1f3c4a
DingDongDeng <tpea999@gmail.com>
2023-12-31 11:59:07 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/utils/TradingTimeContext.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/utils/TradingTimeContext.kt
@@ -1,17 +1,11 @@
 package com.dingdongdeng.autotrading.infra.common.utils
 
 import java.time.LocalDateTime
-import java.util.Objects
 
 object TradingTimeContext {
     private val contextThreadLocal = ThreadLocal.withInitial { Context() }
     fun now(): LocalDateTime {
         val context = contextThreadLocal.get()
-        if (Objects.isNull(context)) {
-            val newContext = Context()
-            contextThreadLocal.set(newContext)
-            return newContext.now()
-        }
         return context.now()
     }
 
