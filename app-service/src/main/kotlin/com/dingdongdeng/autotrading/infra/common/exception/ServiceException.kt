edaab9b017b6d4e45297bfd847e3a1200dfda705
DingDongDeng <tpea999@gmail.com>
2024-01-07 00:03:20 +0900

    작성중

---
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/exception/ServiceException.kt
@@ -0,0 +1,17 @@
+package com.dingdongdeng.autotrading.infra.common.exception
+
+import org.springframework.http.HttpStatus
+
+class CriticalException(
+    val status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    val systemMessage: String = "시스템 오류",
+    val userMessage: String = "시스템 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+    val throwable: Throwable? = null,
+) : RuntimeException(systemMessage, throwable)
+
+class WarnException(
+    val status: HttpStatus = HttpStatus.BAD_REQUEST,
+    val systemMessage: String = "시스템 오류",
+    val userMessage: String = "시스템 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+    val throwable: Throwable,
+) : RuntimeException(systemMessage, throwable)
---
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/presentation/common/advice/GlobalExceptionHandler.kt
@@ -0,0 +1,46 @@
+package com.dingdongdeng.autotrading.presentation.common.advice
+
+import com.dingdongdeng.autotrading.infra.client.slack.SlackSender
+import com.dingdongdeng.autotrading.infra.common.exception.CriticalException
+import com.dingdongdeng.autotrading.infra.common.exception.WarnException
+import com.dingdongdeng.autotrading.infra.common.log.Slf4j.Companion.log
+import com.dingdongdeng.autotrading.presentation.common.CommonResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler(
+    private val slackSender: SlackSender,
+) {
+
+    @ExceptionHandler(WarnException::class)
+    fun handler(e: WarnException): ResponseEntity<CommonResponse<Unit>> {
+        log.warn(e.systemMessage, e)
+        return ResponseEntity(
+            CommonResponse(message = e.userMessage),
+            e.status,
+        )
+    }
+
+    @ExceptionHandler(CriticalException::class)
+    fun handler(e: CriticalException): ResponseEntity<CommonResponse<Unit>> {
+        log.error(e.systemMessage, e)
+        slackSender.send(e)
+        return ResponseEntity(
+            CommonResponse(message = e.userMessage),
+            e.status,
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handler(e: Exception): ResponseEntity<CommonResponse<Unit>> {
+        log.error(e.message, e)
+        slackSender.send(e)
+        return ResponseEntity(
+            CommonResponse(message = "시스템 에러 발생"),
+            HttpStatus.INTERNAL_SERVER_ERROR,
+        )
+    }
+}
