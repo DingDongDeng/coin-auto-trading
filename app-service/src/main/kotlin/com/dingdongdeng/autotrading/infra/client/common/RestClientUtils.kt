0bedea76b991f72cee41f6e0dcf565e812e4115f
DingDongDeng <tpea999@gmail.com>
2024-01-07 20:15:49 +0900

    RestClient에 로깅 인터셉터 추가

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/client/common/RestClientUtils.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/client/common/RestClientUtils.kt
@@ -1,11 +1,20 @@
 package com.dingdongdeng.autotrading.infra.client.common
 
+import com.dingdongdeng.autotrading.infra.common.log.Slf4j.Companion.log
 import org.springframework.boot.web.client.ClientHttpRequestFactories
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpRequest
 import org.springframework.http.MediaType
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.util.StreamUtils
 import org.springframework.web.client.RestClient
+import java.nio.charset.Charset
 import java.time.Duration
+
 
 object RestClientUtils {
 
@@ -17,7 +26,6 @@ object RestClientUtils {
     fun makeRestClient(baseUrl: String, readTimeout: Long, connectionTimeout: Long): RestClient {
         //FIXME
         // MDC 컨텍스트는 잘 유지될까?
-        // 요청,응답 로깅은 어떻게 해야할까?
 
         val requestFactory = ClientHttpRequestFactories.get(
             ClientHttpRequestFactorySettings.DEFAULTS
@@ -27,13 +35,64 @@ object RestClientUtils {
 
         return RestClient.builder()
             .baseUrl(baseUrl)
-            .requestFactory(requestFactory)
+            .requestFactory(BufferingClientHttpRequestFactory(requestFactory))
             .defaultHeaders { httpHeaders ->
                 httpHeaders[HttpHeaders.CONTENT_TYPE] = MediaType.APPLICATION_JSON_VALUE
             }
             .requestInterceptors {
-                //TODO 로깅 intercepter를 정의... body 여러번 읽을수있나?
+                it.add(LoggingInterceptor())
             }
             .build()
+    }
+
+    // https://medium.com/javarevisited/spring-resttemplate-request-response-logging-f021be66c2c0
+    private class LoggingInterceptor : ClientHttpRequestInterceptor {
+
+        override fun intercept(
+            request: HttpRequest,
+            body: ByteArray,
+            execution: ClientHttpRequestExecution
+        ): ClientHttpResponse {
+            logRequest(request, body)
+            val response = execution.execute(request, body)
+            logResponse(request, response)
+            return response
+        }
+
+        private fun logRequest(request: HttpRequest, body: ByteArray) {
+            log.info(
+                """
+                ===log request start=== 
+                URI: {},
+                Method: {},
+                Headers: {},
+                Request body: {},
+                ===log request end=== 
+                """.trimIndent(),
+                request.uri,
+                request.method,
+                request.headers,
+                String(body, charset("UTF-8"))
+            )
+        }
+
+        private fun logResponse(request: HttpRequest, response: ClientHttpResponse) {
+            log.info(
+                """
+                ===log response start===
+                URI: {},
+                Status code: {},
+                Status text: {},
+                Headers: {},
+                Response body: {},
+                ===log response end===
+                """.trimIndent(),
+                request.uri,
+                response.statusCode,
+                response.statusText,
+                response.headers,
+                StreamUtils.copyToString(response.body, Charset.defaultCharset())
+            )
+        }
     }
 }
