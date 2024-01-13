06e033638ec134a27b67d298c59ee4c7faca272c
DingDongDeng <tpea999@gmail.com>
2024-01-13 21:36:14 +0900

    작성중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/service/BackTestSpotCoinExchangeService.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/exchange/service/BackTestSpotCoinExchangeService.kt
@@ -5,23 +5,41 @@ import com.dingdongdeng.autotrading.domain.exchange.model.ExchangeKeyPair
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeChartParam
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeOrder
 import com.dingdongdeng.autotrading.domain.exchange.model.SpotCoinExchangeOrderParam
+import com.dingdongdeng.autotrading.domain.exchange.repository.ExchangeCandleRepository
+import com.dingdongdeng.autotrading.infra.common.exception.WarnException
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
+import com.dingdongdeng.autotrading.infra.common.type.TradeState
+import com.dingdongdeng.autotrading.infra.common.utils.TimeContext
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class BackTestSpotCoinExchangeService(
+    private val exchangeCandleRepository: ExchangeCandleRepository,
 ) : SpotCoinExchangeService {
 
     override fun order(param: SpotCoinExchangeOrderParam, keyParam: ExchangeKeyPair): SpotCoinExchangeOrder {
-        TODO()
+        return SpotCoinExchangeOrder(
+            orderId = UUID.randomUUID().toString(),
+            orderType = param.orderType,
+            priceType = param.priceType,
+            price = param.price,
+            volume = param.volume,
+            tradeState = TradeState.DONE,
+            exchangeType = EXCHANGE_TYPE,
+            coinType = param.coinType,
+            fee = param.volume * param.price * (0.05 / 100), // upbit 수수료 0.05% 적용
+            orderDateTime = TimeContext.now(),
+            cancelDateTime = null,
+        )
     }
 
     override fun cancel(orderId: String, keyParam: ExchangeKeyPair): SpotCoinExchangeOrder {
-        TODO()
+        throw WarnException(userMessage = "백테스트에서는 지원하지 않는 기능입니다. (모든 주문이 즉시 DONE 상태가 됩니다)")
     }
 
     override fun getOrder(orderId: String, keyParam: ExchangeKeyPair): SpotCoinExchangeOrder {
-        TODO()
+        throw WarnException(userMessage = "백테스트에서는 지원하지 않는 기능입니다. (모든 주문이 즉시 DONE 상태가 됩니다)")
     }
 
     // from <= 조회범위 <= to
@@ -29,12 +47,19 @@ class BackTestSpotCoinExchangeService(
         TODO()
     }
 
+    override fun loadChart(param: SpotCoinExchangeChartParam, keyParam: ExchangeKeyPair) {
+        throw WarnException(userMessage = "백테스트에서는 지원하지 않는 기능입니다.")
+    }
+
     override fun getKeyPair(keyPairId: String): ExchangeKeyPair {
-        TODO()
+        return ExchangeKeyPair(
+            accessKey = "",
+            secretKey = "",
+        )
     }
 
     override fun registerKeyPair(accessKey: String, secretKey: String, userId: Long): String {
-        TODO()
+        return ""
     }
 
     override fun support(exchangeType: ExchangeType): Boolean {
