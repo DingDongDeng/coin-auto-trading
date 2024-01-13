ab141d4af8bdf770b65239a313c88313dea76367
DingDongDeng <tpea999@gmail.com>
2024-01-14 02:28:11 +0900

    작성 중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/trade/repository/CoinTradeHistoryRepository.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/trade/repository/CoinTradeHistoryRepository.kt
@@ -4,11 +4,22 @@ import com.dingdongdeng.autotrading.domain.trade.entity.CoinTradeHistory
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import com.dingdongdeng.autotrading.infra.common.type.TradeState
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface CoinTradeHistoryRepository : JpaRepository<CoinTradeHistory, Long> {
 
-    fun findByProcessorIdAndCoinTypeAndStateInOrderByTradedAtAsc(
-        autoTradeProcessorId: String,
+    @Query(
+        """
+        select history 
+        from CoinTradeHistory history
+        where history.processorId = :processorId
+        and history.coinType = :coinType
+        and history.state in :states
+        order by history.tradedAt
+    """
+    )
+    fun findAllCoinTradeHistories(
+        processorId: String,
         coinType: CoinType,
         states: List<TradeState>,
     ): List<CoinTradeHistory>
