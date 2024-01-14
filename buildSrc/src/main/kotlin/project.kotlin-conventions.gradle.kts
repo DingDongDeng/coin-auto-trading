f7eb41d10bb2f4aa9431a0085318fe510afec26d
DingDongDeng <tpea999@gmail.com>
2024-01-14 12:31:09 +0900

    작성 중

--- buildSrc/src/main/kotlin/project.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/project.kotlin-conventions.gradle.kts
@@ -33,4 +33,5 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
 }
