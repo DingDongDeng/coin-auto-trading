60ac267b6b2d5e9a4751fc40384f62451da0b9be
DingDongDeng <tpea999@gmail.com>
2024-02-03 23:50:42 +0900

    작업중

---
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/CoinStrategyService.kt
@@ -0,0 +1,25 @@
+package com.dingdongdeng.autotrading.domain.strategy.service
+
+import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyMakeTaskParam
+import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyTask
+import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
+import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
+import org.springframework.stereotype.Service
+
+@Service
+class CoinStrategyService(
+    private val strategies: List<SpotCoinStrategy>,
+) {
+
+    fun getTask(
+        params: List<SpotCoinStrategyMakeTaskParam>,
+        config: Map<String, Any>,
+        strategyType: CoinStrategyType,
+        autoTradeProcessorId: String,
+        keyPairId: String,
+        exchangeType: ExchangeType,
+    ): List<SpotCoinStrategyTask> {
+        val strategyService = strategies.first { it.support(strategyType) }
+        return strategyService.makeTask(params, config)
+    }
+}
