8a25e90eb034751d8b515a2c80bf77f91084d91c
DingDongDeng <tpea999@gmail.com>
2024-02-04 11:53:03 +0900

    작업중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/autotrade/service/AutoTradeService.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/autotrade/service/AutoTradeService.kt
@@ -1,6 +1,7 @@
 package com.dingdongdeng.autotrading.domain.autotrade.service
 
 import com.dingdongdeng.autotrading.domain.autotrade.model.CoinAutoTradeProcessor
+import com.dingdongdeng.autotrading.domain.autotrade.model.CoinBackTestProcessor
 import com.dingdongdeng.autotrading.domain.chart.service.ChartService
 import com.dingdongdeng.autotrading.domain.process.service.ProcessService
 import com.dingdongdeng.autotrading.domain.strategy.service.CoinStrategyService
@@ -11,7 +12,7 @@ import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.time.LocalDateTime
 
 @Service
 class AutoTradeService(
@@ -33,8 +34,7 @@ class AutoTradeService(
     ): String {
         return processService.register(
             CoinAutoTradeProcessor(
-                _id = "AUTOTRADE-${UUID.randomUUID()}",
-                _userId = userId,
+                userId = userId,
                 coinStrategyType = coinStrategyType,
                 exchangeType = exchangeType,
                 coinTypes = coinTypes,
@@ -47,9 +47,45 @@ class AutoTradeService(
                 chartService = chartService,
                 coinTradeService = coinTradeService,
                 coinStrategyService = coinStrategyService,
-
-                )
+            )
         )
+    }
+
+    fun backTest(
+        startDateTime: LocalDateTime,
+        endDateTime: LocalDateTime,
+        durationUnit: CandleUnit, // 백테스트 시간 간격
+        userId: Long,
+        coinStrategyType: CoinStrategyType,
+        coinTypes: List<CoinType>,
+        candleUnits: List<CandleUnit>,
+        config: Map<String, Any>,
+    ): String {
+        val autoTradeProcessor = CoinAutoTradeProcessor(
+            userId = userId,
+            coinStrategyType = coinStrategyType,
+            exchangeType = ExchangeType.BACKTEST,
+            coinTypes = coinTypes,
+            candleUnits = candleUnits,
+            keyPairId = "",
+            config = config,
+            duration = 0,
+            slackSender = slackSender,
+
+            chartService = chartService,
+            coinTradeService = coinTradeService,
+            coinStrategyService = coinStrategyService,
+        )
+        val backTestProcessor = CoinBackTestProcessor(
+            userId = userId,
+            startDateTime = startDateTime,
+            endDateTime = endDateTime,
+            durationUnit = durationUnit,
+            autoTradeProcessor = autoTradeProcessor,
+        )
+        processService.register(backTestProcessor)
+        processService.start(backTestProcessor.id)
+        return backTestProcessor.id
     }
 
     fun start(autoTradeProcessorId: String) {
