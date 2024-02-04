76d122a23c2861180cbc2329c52942630dc885ac
DingDongDeng <tpea999@gmail.com>
2024-02-04 21:45:37 +0900

    스켈레톤 전략 생성

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/SpotCoinStrategySkeleton.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/SpotCoinStrategySkeleton.kt
@@ -3,76 +3,81 @@ package com.dingdongdeng.autotrading.domain.strategy.service
 import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyMakeTaskParam
 import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyTask
 
-abstract class SpotCoinStrategySkeleton : SpotCoinStrategy {
+abstract class SpotCoinStrategySkeleton<T> : SpotCoinStrategy {
 
     override fun makeTask(
         params: List<SpotCoinStrategyMakeTaskParam>,
         config: Map<String, Any>
     ): List<SpotCoinStrategyTask> {
-        if (whenWaitTrades(params, config)) {
-            return thenWaitTrades(params, config)
+        val convertedConfig = convertConfig(config)
+        if (whenWaitTrades(params, convertedConfig)) {
+            return thenWaitTrades(params, convertedConfig)
         }
 
-        if (whenBuyTrade(params, config)) {
-            return thenBuyTrade(params, config)
+        if (whenBuyTrade(params, convertedConfig)) {
+            return thenBuyTrade(params, convertedConfig)
         }
 
-        if (whenProfitTrade(params, config)) {
-            return thenProfitTrade(params, config)
+        if (whenProfitTrade(params, convertedConfig)) {
+            return thenProfitTrade(params, convertedConfig)
         }
 
-        if (whenLossTrade(params, config)) {
-            return thenLossTrade(params, config)
+        if (whenLossTrade(params, convertedConfig)) {
+            return thenLossTrade(params, convertedConfig)
         }
 
         return emptyList()
     }
 
+    abstract fun convertConfig(
+        config: Map<String, Any>
+    ): T
+
     // 미체결 주문이 존재할때
     abstract fun whenWaitTrades(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): Boolean
 
     // 미체결 주문이 존재할때 어떻게 할 것 인지
     abstract fun thenWaitTrades(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): List<SpotCoinStrategyTask>
 
     // 매수 주문을 해야할때
     abstract fun whenBuyTrade(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): Boolean
 
     // 매수 주문을 어떻게 할 것 인지
     abstract fun thenBuyTrade(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): List<SpotCoinStrategyTask>
 
     // 익절 주문을 해야할때
     abstract fun whenProfitTrade(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): Boolean
 
     // 익절 주문을 어떻게 할 것 인지
     abstract fun thenProfitTrade(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): List<SpotCoinStrategyTask>
 
     // 손절 주문을 해야할 때
     abstract fun whenLossTrade(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): Boolean
 
     // 손절 주문을 어떻게 할 것 인지
     abstract fun thenLossTrade(
         param: List<SpotCoinStrategyMakeTaskParam>,
-        config: Map<String, Any>
+        config: T
     ): List<SpotCoinStrategyTask>
 }
