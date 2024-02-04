113addfd55ab741f8ab2cf6c2cd1f98a20dbc158
DingDongDeng <tpea999@gmail.com>
2024-02-04 21:50:11 +0900

    스켈레톤 전략 생성

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/SpotCoinStrategySkeleton.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/service/SpotCoinStrategySkeleton.kt
@@ -35,49 +35,49 @@ abstract class SpotCoinStrategySkeleton<T> : SpotCoinStrategy {
 
     // 미체결 주문이 존재할때
     abstract fun whenWaitTrades(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): Boolean
 
     // 미체결 주문이 존재할때 어떻게 할 것 인지
     abstract fun thenWaitTrades(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): List<SpotCoinStrategyTask>
 
     // 매수 주문을 해야할때
     abstract fun whenBuyTrade(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): Boolean
 
     // 매수 주문을 어떻게 할 것 인지
     abstract fun thenBuyTrade(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): List<SpotCoinStrategyTask>
 
     // 익절 주문을 해야할때
     abstract fun whenProfitTrade(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): Boolean
 
     // 익절 주문을 어떻게 할 것 인지
     abstract fun thenProfitTrade(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): List<SpotCoinStrategyTask>
 
     // 손절 주문을 해야할 때
     abstract fun whenLossTrade(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): Boolean
 
     // 손절 주문을 어떻게 할 것 인지
     abstract fun thenLossTrade(
-        param: List<SpotCoinStrategyMakeTaskParam>,
+        params: List<SpotCoinStrategyMakeTaskParam>,
         config: T
     ): List<SpotCoinStrategyTask>
 }
