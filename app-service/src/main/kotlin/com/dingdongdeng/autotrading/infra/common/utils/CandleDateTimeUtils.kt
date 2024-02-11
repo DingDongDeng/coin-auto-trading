7db871cbc0cb8b98e908a29855c585a6f146f945
DingDongDeng <tpea999@gmail.com>
2024-02-12 01:29:30 +0900

    작업중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/utils/CandleDateTimeUtils.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/utils/CandleDateTimeUtils.kt
@@ -4,24 +4,8 @@ import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.LocalTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import kotlin.math.abs
-
-
-val yyyyMMddHHmmss: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-
-fun LocalDateTime.convertToString(): String {
-    return this.format(yyyyMMddHHmmss)
-}
-
-fun LocalDateTime.toUtc(): LocalDateTime {
-    return this // 마지막 캔들 시각 (비우면 가장 최근 시각), UTC 기준
-        .atZone(ZoneId.of("Asia/Seoul"))
-        .withZoneSameInstant(ZoneId.of("UTC"))
-        .toLocalDateTime()
-}
 
 object CandleDateTimeUtils {
 
---
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/utils/LocalDateTimeUtils.kt
@@ -0,0 +1,27 @@
+package com.dingdongdeng.autotrading.infra.common.utils
+
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+
+val yyyyMMddHHmmss: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+fun LocalDateTime.convertToString(): String {
+    return this.format(yyyyMMddHHmmss)
+}
+
+fun LocalDateTime.toUtc(): LocalDateTime {
+    return this // 마지막 캔들 시각 (비우면 가장 최근 시각), UTC 기준
+        .atZone(ZoneId.of("Asia/Seoul"))
+        .withZoneSameInstant(ZoneId.of("UTC"))
+        .toLocalDateTime()
+}
+
+fun minDate(d1: LocalDateTime, d2: LocalDateTime): LocalDateTime {
+    return if (d1 < d2) {
+        return d1
+    } else {
+        d2
+    }
+}
