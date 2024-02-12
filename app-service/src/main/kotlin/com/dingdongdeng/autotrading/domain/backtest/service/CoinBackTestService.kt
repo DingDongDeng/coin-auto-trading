862b3633b18c9769f4efc7ae8e6301c878e36ee2
DingDongDeng <tpea999@gmail.com>
2024-02-12 14:46:20 +0900

    작업중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/service/CoinBackTestService.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/service/CoinBackTestService.kt
@@ -8,6 +8,7 @@ import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
 import com.dingdongdeng.autotrading.domain.trade.service.CoinTradeService
 import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
+import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -25,6 +26,7 @@ class CoinBackTestService(
         durationUnit: CandleUnit, // 백테스트 시간 간격
         userId: Long,
         coinStrategyType: CoinStrategyType,
+        exchangeType: ExchangeType,
         coinTypes: List<CoinType>,
         candleUnits: List<CandleUnit>,
         config: Map<String, Any>,
@@ -35,6 +37,7 @@ class CoinBackTestService(
             endDateTime = endDateTime,
             durationUnit = durationUnit,
             coinStrategyType = coinStrategyType,
+            exchangeType = exchangeType,
             coinTypes = coinTypes,
             candleUnits = candleUnits,
             config = config,
