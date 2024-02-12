51289b72061a6919b4e4fb2b9a453e78ed6b79e5
DingDongDeng <tpea999@gmail.com>
2024-02-12 14:30:06 +0900

    작업중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/model/AvailBackTestRange.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/model/AvailBackTestRange.kt
@@ -1,16 +1,18 @@
 package com.dingdongdeng.autotrading.domain.backtest.model
 
 import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
+import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
 import java.time.LocalDateTime
 
 data class AvailBackTestRange(
     val exchangeType: ExchangeType,
+    val coinType: CoinType,
     val candleUnit: CandleUnit,
     val startDateTime: LocalDateTime,
     val endDateTime: LocalDateTime,
 ) {
-    fun isRanged(startDateTime: LocalDateTime, endDateTime: LocalDateTime): Boolean {
-        return this.startDateTime <= startDateTime && this.endDateTime >= endDateTime
+    fun isRanged(coinType: CoinType, startDateTime: LocalDateTime, endDateTime: LocalDateTime): Boolean {
+        return this.coinType == coinType && this.startDateTime <= startDateTime && this.endDateTime >= endDateTime
     }
 }
