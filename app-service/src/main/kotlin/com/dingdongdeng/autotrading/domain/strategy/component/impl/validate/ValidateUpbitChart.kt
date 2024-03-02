16a949fd8bfe2a82bcb8f90890ca1acbda5d013d
DingDongDeng <tpea999@gmail.com>
2024-03-02 20:54:13 +0900

    작업중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/component/impl/validate/ValidateUpbitChart.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/component/impl/validate/ValidateUpbitChart.kt
@@ -23,6 +23,9 @@ class ValidateUpbitChart : SpotCoinStrategy {
         params: List<SpotCoinStrategyMakeTaskParam>,
         config: Map<String, Any>
     ): List<SpotCoinStrategyTask> {
+        /**
+         * 2024-02-27 ~ 2024-02-29 기간에 대한 임의 검증
+         */
         log.info("validate upbit chart ... ${TimeContext.now()}")
         params.all { it.exchangeType == ExchangeType.UPBIT }
         validateBitCoin(params.first { it.coinType == CoinType.BITCOIN })
@@ -35,80 +38,158 @@ class ValidateUpbitChart : SpotCoinStrategy {
 
     private fun validateBitCoin(param: SpotCoinStrategyMakeTaskParam) {
         val now = TimeContext.now()
-        val unit1M = CandleUnit.UNIT_1M
-        val chart1M = param.getChart(unit1M)
-        when (CandleDateTimeUtils.makeUnitDateTime(now, unit1M)) {
+
+        validate1M(now, param)
+        validate15M(now, param)
+    }
+
+    private fun validate1M(now: LocalDateTime, param: SpotCoinStrategyMakeTaskParam) {
+        val unit = CandleUnit.UNIT_1M
+        val chart = param.getChart(unit)
+        when (CandleDateTimeUtils.makeUnitDateTime(now, unit)) {
             LocalDateTime.of(2024, 2, 29, 22, 30) -> {
-                val candle = chart1M.candles.last()
+                val candle = chart.candles.last()
                 val indicators = candle.indicators
                 if (candle.closingPrice != 87416000.0) {
-                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit1M closingPrice=${candle.closingPrice}")
+                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit closingPrice=${candle.closingPrice}")
                 }
                 if (candle.indicators.rsi.round(4.0) != 0.4634) {
-                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit1M, rsi=${candle.indicators.rsi}")
+                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit, rsi=${candle.indicators.rsi}")
                 }
                 if (indicators.ma.sma120.round() != 87817942.0) {
-                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit1M, sma120=${indicators.ma.sma120}")
+                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit, sma120=${indicators.ma.sma120}")
                 }
                 if (indicators.bollingerBands.upper.round() != 88058560.0) {
-                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit1M, boll.upper=${indicators.bollingerBands.upper}")
+                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit, boll.upper=${indicators.bollingerBands.upper}")
                 }
                 if (indicators.bollingerBands.lower.round() != 87038340.0) {
-                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit1M, boll.lower=${indicators.bollingerBands.lower}")
+                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit, boll.lower=${indicators.bollingerBands.lower}")
                 }
                 if (indicators.macd.signal.floor() != -78959.0) {
-                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit1M, macd.signal=${indicators.macd.signal}")
+                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit, macd.signal=${indicators.macd.signal}")
                 }
             }
 
             LocalDateTime.of(2024, 2, 28, 22, 27) -> {
-                val candle = chart1M.candles.last()
+                val candle = chart.candles.last()
                 val indicators = candle.indicators
                 if (candle.closingPrice != 84185000.0) {
-                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit1M closingPrice=${candle.closingPrice}")
+                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit closingPrice=${candle.closingPrice}")
                 }
                 if (candle.indicators.rsi.round(4.0) != 0.7163) {
-                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit1M, rsi=${candle.indicators.rsi}")
+                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit, rsi=${candle.indicators.rsi}")
                 }
                 if (indicators.ma.sma120 != 82498675.0) {
-                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit1M, sma120=${indicators.ma.sma120}")
+                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit, sma120=${indicators.ma.sma120}")
                 }
                 if (indicators.bollingerBands.upper.round() != 84320311.0) {
-                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit1M, boll.upper=${indicators.bollingerBands.upper}")
+                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit, boll.upper=${indicators.bollingerBands.upper}")
                 }
                 if (indicators.bollingerBands.lower.round() != 82441989.0) {
-                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit1M, boll.lower=${indicators.bollingerBands.lower}")
+                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit, boll.lower=${indicators.bollingerBands.lower}")
                 }
                 if (indicators.macd.signal.round() != 268222.0) {
-                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit1M, macd.signal=${indicators.macd.signal}")
+                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit, macd.signal=${indicators.macd.signal}")
                 }
             }
 
             LocalDateTime.of(2024, 2, 27, 19, 39) -> {
-                val candle = chart1M.candles.last()
+                val candle = chart.candles.last()
                 val indicators = candle.indicators
                 if (candle.closingPrice != 77652000.0) {
-                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit1M closingPrice=${candle.closingPrice}")
+                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit closingPrice=${candle.closingPrice}")
                 }
                 if (candle.indicators.rsi.round(4.0) != 0.3272) {
-                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit1M, rsi=${candle.indicators.rsi}")
+                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit, rsi=${candle.indicators.rsi}")
                 }
                 if (indicators.ma.sma120 != 77573200.0) {
-                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit1M, sma120=${indicators.ma.sma120}")
+                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit, sma120=${indicators.ma.sma120}")
                 }
                 if (indicators.bollingerBands.upper.round() != 78058005.0) {
-                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit1M, boll.upper=${indicators.bollingerBands.upper}")
+                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit, boll.upper=${indicators.bollingerBands.upper}")
                 }
                 if (indicators.bollingerBands.lower.round() != 77648595.0) {
-                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit1M, boll.lower=${indicators.bollingerBands.lower}")
+                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit, boll.lower=${indicators.bollingerBands.lower}")
                 }
                 if (indicators.macd.signal.round() != 3594.0) {
-                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit1M, macd.signal=${indicators.macd.signal}")
+                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit, macd.signal=${indicators.macd.signal}")
                 }
             }
         }
+    }
 
-        val chart15M = param.getChart(CandleUnit.UNIT_15M)
+    private fun validate15M(now: LocalDateTime, param: SpotCoinStrategyMakeTaskParam) {
+        val unit = CandleUnit.UNIT_15M
+        val chart = param.getChart(unit)
+        when (CandleDateTimeUtils.makeUnitDateTime(now, unit)) {
+            LocalDateTime.of(2024, 2, 29, 22, 30) -> {
+                val candle = chart.candles.last()
+                val indicators = candle.indicators
+                if (candle.closingPrice != 87416000.0) {
+                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit closingPrice=${candle.closingPrice}")
+                }
+                if (candle.indicators.rsi.round(4.0) != 0.4634) {
+                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit, rsi=${candle.indicators.rsi}")
+                }
+                if (indicators.ma.sma120.round() != 87817942.0) {
+                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit, sma120=${indicators.ma.sma120}")
+                }
+                if (indicators.bollingerBands.upper.round() != 88058560.0) {
+                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit, boll.upper=${indicators.bollingerBands.upper}")
+                }
+                if (indicators.bollingerBands.lower.round() != 87038340.0) {
+                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit, boll.lower=${indicators.bollingerBands.lower}")
+                }
+                if (indicators.macd.signal.floor() != -78959.0) {
+                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit, macd.signal=${indicators.macd.signal}")
+                }
+            }
 
+            LocalDateTime.of(2024, 2, 28, 22, 27) -> {
+                val candle = chart.candles.last()
+                val indicators = candle.indicators
+                if (candle.closingPrice != 84185000.0) {
+                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit closingPrice=${candle.closingPrice}")
+                }
+                if (candle.indicators.rsi.round(4.0) != 0.7163) {
+                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit, rsi=${candle.indicators.rsi}")
+                }
+                if (indicators.ma.sma120 != 82498675.0) {
+                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit, sma120=${indicators.ma.sma120}")
+                }
+                if (indicators.bollingerBands.upper.round() != 84320311.0) {
+                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit, boll.upper=${indicators.bollingerBands.upper}")
+                }
+                if (indicators.bollingerBands.lower.round() != 82441989.0) {
+                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit, boll.lower=${indicators.bollingerBands.lower}")
+                }
+                if (indicators.macd.signal.round() != 268222.0) {
+                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit, macd.signal=${indicators.macd.signal}")
+                }
+            }
+
+            LocalDateTime.of(2024, 2, 27, 19, 39) -> {
+                val candle = chart.candles.last()
+                val indicators = candle.indicators
+                if (candle.closingPrice != 77652000.0) {
+                    throw CriticalException.of("종가 일치하지 않음, now=$now, unit=$unit closingPrice=${candle.closingPrice}")
+                }
+                if (candle.indicators.rsi.round(4.0) != 0.3272) {
+                    throw CriticalException.of("RSI 일치하지 않음, now=$now, unit=$unit, rsi=${candle.indicators.rsi}")
+                }
+                if (indicators.ma.sma120 != 77573200.0) {
+                    throw CriticalException.of("sma120 일치하지 않음, now=$now, unit=$unit, sma120=${indicators.ma.sma120}")
+                }
+                if (indicators.bollingerBands.upper.round() != 78058005.0) {
+                    throw CriticalException.of("boll.upper 일치하지 않음, now=$now, unit=$unit, boll.upper=${indicators.bollingerBands.upper}")
+                }
+                if (indicators.bollingerBands.lower.round() != 77648595.0) {
+                    throw CriticalException.of("boll.lower 일치하지 않음, now=$now, unit=$unit, boll.lower=${indicators.bollingerBands.lower}")
+                }
+                if (indicators.macd.signal.round() != 3594.0) {
+                    throw CriticalException.of("macd.signal 일치하지 않음, now=$now, unit=$unit, macd.signal=${indicators.macd.signal}")
+                }
+            }
+        }
     }
 }
