835203921c56e41595985784625ce6207a6270e9
DingDongDeng <tpea999@gmail.com>
2024-03-03 20:38:35 +0900

    sss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/type/CoinStrategyType.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/type/CoinStrategyType.kt
@@ -3,7 +3,7 @@ package com.dingdongdeng.autotrading.domain.strategy.type
 enum class CoinStrategyType(
     val desc: String,
 ) {
-    EXAMPLE("코인 현물 전략 예제"),
+    EXAMPLE("코인 현물 전략 예제(RSI 매매)"),
     UPBIT_CHART_VALIDATE("백테스트 환경 교차 검증을 위한 업비트 차트 임의 구간 비교"),
     ;
 }
