1a1783eb0bfcb5e811b3029c30892adbaf7b6c83
DingDongDeng <tpea999@gmail.com>
2024-03-03 20:41:45 +0900

    sss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/component/impl/ExampleSpotCoinStrategy.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/component/impl/ExampleSpotCoinStrategy.kt
@@ -5,8 +5,9 @@ import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyMakeTa
 import com.dingdongdeng.autotrading.domain.strategy.model.SpotCoinStrategyTask
 import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
 import com.dingdongdeng.autotrading.infra.common.log.Slf4j.Companion.log
-import com.dingdongdeng.autotrading.infra.common.type.OrderType
+import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.utils.TimeContext
+import com.dingdongdeng.autotrading.infra.common.utils.round
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 
@@ -24,7 +25,7 @@ class ExampleSpotCoinStrategy(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): Boolean {
-        return param.tradeInfo.existsWaitTrade()
+        return param.tradeInfo.existsWaitTrade
     }
 
     override fun thenWaitTrades(
@@ -33,13 +34,12 @@ class ExampleSpotCoinStrategy(
     ): List<SpotCoinStrategyTask> {
         return param.tradeInfo.getOldWaitTrades(30L)
             .map {
-                SpotCoinStrategyTask(
+                SpotCoinStrategyTask.ofCancel(
+                    orderId = it.orderId,
                     coinType = it.coinType,
                     volume = it.volume,
                     price = it.price,
-                    orderType = OrderType.CANCEL,
                     priceType = it.priceType,
-                    orderId = it.orderId,
                 )
             }
     }
@@ -48,42 +48,80 @@ class ExampleSpotCoinStrategy(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): Boolean {
-        return false
+        if (param.tradeInfo.hasVolume) {
+            return false
+        }
+        val chart15M = param.getChart(CandleUnit.UNIT_15M)
+        val rsi = chart15M.last(2).indicators.rsi
+        return rsi < 0.3
     }
 
     override fun thenBuyTrade(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): List<SpotCoinStrategyTask> {
-        return emptyList()
+        val chart15M = param.getChart(CandleUnit.UNIT_15M)
+        return listOf(
+            SpotCoinStrategyTask.ofBuyLimit(
+                coinType = param.coinType,
+                volume = (config.onceTradeAmount / chart15M.currentPrice).round(4.0),
+                price = chart15M.currentPrice,
+            )
+        )
     }
 
     override fun whenProfitTrade(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): Boolean {
-        return false
+        val chart15M = param.getChart(CandleUnit.UNIT_15M)
+        if (param.tradeInfo.profitRate <= 0) {
+            return false
+        }
+        if (chart15M.last(1).indicators.rsi < 0.65) {
+            return false
+        }
+        return true
     }
 
     override fun thenProfitTrade(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): List<SpotCoinStrategyTask> {
-        return emptyList()
+        return listOf(
+            SpotCoinStrategyTask.ofSellLimit(
+                coinType = param.coinType,
+                volume = param.tradeInfo.volume,
+                price = param.tradeInfo.currentPrice,
+            )
+        )
     }
 
     override fun whenLossTrade(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): Boolean {
-        return false
+        val chart15M = param.getChart(CandleUnit.UNIT_15M)
+        if (param.tradeInfo.profitRate >= 0) {
+            return false
+        }
+        if (chart15M.last(1).indicators.rsi > 0.30) {
+            return false
+        }
+        return true
     }
 
     override fun thenLossTrade(
         param: SpotCoinStrategyMakeTaskParam,
         config: ExampleSpotCoinStrategyConfig
     ): List<SpotCoinStrategyTask> {
-        return emptyList()
+        return listOf(
+            SpotCoinStrategyTask.ofSellLimit(
+                coinType = param.coinType,
+                volume = param.tradeInfo.volume,
+                price = param.tradeInfo.currentPrice,
+            )
+        )
     }
 
     override fun support(param: CoinStrategyType): Boolean {
@@ -92,6 +130,5 @@ class ExampleSpotCoinStrategy(
 }
 
 data class ExampleSpotCoinStrategyConfig(
-    val configA: String,
-    val configB: Int,
+    val onceTradeAmount: Int,
 )
