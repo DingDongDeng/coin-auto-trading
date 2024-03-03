c0247db50b258d9dbc245c977a2435c304a8853a
DingDongDeng <tpea999@gmail.com>
2024-03-03 22:40:20 +0900

    ss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/trade/model/CoinTradeInfo.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/trade/model/CoinTradeInfo.kt
@@ -5,11 +5,20 @@ import com.dingdongdeng.autotrading.infra.common.utils.round
 import java.time.LocalDateTime
 
 data class CoinTradeInfo(
-    val volume: Double,     // 보유 수량        ex) 이더리움 1.38개 보유
-    val averagePrice: Double,  // 평균 단가    ex) 이더리움 평균 단가 807,302원
     val currentPrice: Double,
     val tradeHistories: List<CoinTradeHistory>,
 ) {
+    val buyTradeHistories = tradeHistories.filter { it.isBuyOrder() }
+    val sellTradeHistories = tradeHistories.filter { it.isSellOrder() }
+
+    val buyVolume = buyTradeHistories.sumOf { it.volume }
+    val buyValue = buyTradeHistories.sumOf { it.price * it.volume }
+    val sellVolume = sellTradeHistories.sumOf { it.volume }
+    val sellValue = sellTradeHistories.sumOf { it.price * it.volume }
+    val volume = buyVolume - sellVolume
+    val value = buyValue - sellValue
+
+    val averagePrice = if (volume == 0.0) 0.0 else (value / volume).round()
     val currentValuePrice = (volume * currentPrice).round()   // 현재 평가 금액   ex) 보유수량 X 현재가
     val averageValuePrice = (volume * averagePrice).round() // 평단가 평가 금액   ex) 보유수량 X 평단가
     val profitPrice = (currentValuePrice - averageValuePrice) // 손익 평가 금액 ex) currentValuePrice - averageValuePrice
@@ -17,7 +26,6 @@ data class CoinTradeInfo(
         if (averageValuePrice == 0.0) 0.0 else ((profitPrice / averageValuePrice) * 100.0).round(2.0)// 수익율 (xx.xx%)
 
     val existsWaitTrade = tradeHistories.any { it.isWait() }
-    val buyTradeHistory = tradeHistories.filter { it.isBuyOrder() }
     val hasVolume = volume != 0.0
     val lastTradedAt = if (tradeHistories.isEmpty()) LocalDateTime.MIN else tradeHistories.last().tradedAt
 
