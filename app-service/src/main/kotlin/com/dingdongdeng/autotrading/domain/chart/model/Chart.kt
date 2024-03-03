43360828bd1fa4c46079209f54ee25635f30ec23
DingDongDeng <tpea999@gmail.com>
2024-03-03 17:16:59 +0900

    sss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/chart/model/Chart.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/chart/model/Chart.kt
@@ -10,5 +10,6 @@ data class Chart(
     val candleUnit: CandleUnit,
     val candles: List<Candle>,
 ) {
-    fun last(n: Int): Candle = candles[candles.size - 1 - n]
+    // 마지막 N번째 요소
+    fun last(idx: Int): Candle = candles[candles.size - 1 - idx]
 }
