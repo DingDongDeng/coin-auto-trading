668d107a81bbd232c092cf2cd5bf4e69164a6d08
DingDongDeng <tpea999@gmail.com>
2024-03-03 17:02:29 +0900

    TTL 추가

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/cache/CacheConfig.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/cache/CacheConfig.kt
@@ -1,18 +1,28 @@
 package com.dingdongdeng.autotrading.infra.cache
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 
 @Configuration
 @EnableCaching
 class CacheConfig {
     @Bean
-    fun cacheManager(): CacheManager {
-        return ConcurrentMapCacheManager(QUERY_CACHE_MANAGER)
+    fun cacheManager(caffeine: Caffeine<Any, Any>): CacheManager {
+        val caffeineCacheManager = CaffeineCacheManager()
+        caffeineCacheManager.setCaffeine(caffeine)
+        return caffeineCacheManager
+    }
+
+    @Bean
+    fun caffeine(): Caffeine<Any, Any> {
+        return Caffeine.newBuilder()
+            .expireAfterWrite(10, TimeUnit.SECONDS)
     }
 
     companion object {
