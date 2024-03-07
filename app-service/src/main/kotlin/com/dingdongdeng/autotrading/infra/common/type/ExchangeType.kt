5f5222f4c624b5fb9d93cebf802aa802eca662bb
DingDongDeng <tpea999@gmail.com>
2024-03-07 21:23:24 +0900

    ss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/type/ExchangeType.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/type/ExchangeType.kt
@@ -1,28 +1,16 @@
 package com.dingdongdeng.autotrading.infra.common.type
 
-import java.util.*
-
 enum class ExchangeType(
     val desc: String,
 ) {
     UPBIT("업비트"),
     BINANCE("바이낸스"),
-    BACKTEST("백테스트 거래소"),
+    BACKTEST_UPBIT("업비트 백테스트")
     ;
 
     companion object {
-        fun of(name: String?): ExchangeType {
-            return values().first { type -> type.name.equals(name, ignoreCase = true) }
-        }
-
-        fun toMap(): EnumMap<ExchangeType, String> {
-            val map = EnumMap<ExchangeType, String>(
-                ExchangeType::class.java
-            )
-            for (value in values()) {
-                map[value] = value.desc
-            }
-            return map
+        fun of(name: String): ExchangeType {
+            return entries.first { type -> type.name.equals(name, ignoreCase = true) }
         }
     }
 }
