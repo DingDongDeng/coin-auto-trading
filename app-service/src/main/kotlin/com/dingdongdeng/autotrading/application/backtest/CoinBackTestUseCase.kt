a9f3bc7d572291b3862ace0adf875deee48c7f6e
DingDongDeng <tpea999@gmail.com>
2024-03-09 01:48:43 +0900

    ss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/application/backtest/CoinBackTestUseCase.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/application/backtest/CoinBackTestUseCase.kt
@@ -3,10 +3,10 @@ package com.dingdongdeng.autotrading.application.backtest
 import com.dingdongdeng.autotrading.domain.autotrade.factory.AutoTradeProcessorFactory
 import com.dingdongdeng.autotrading.domain.backtest.factory.BackTestProcessorFactory
 import com.dingdongdeng.autotrading.domain.backtest.model.CoinBackTestProcessor
-import com.dingdongdeng.autotrading.domain.chart.service.CoinChartService
+import com.dingdongdeng.autotrading.domain.backtest.model.CoinBackTestResult
+import com.dingdongdeng.autotrading.domain.backtest.service.CoinBackTestService
 import com.dingdongdeng.autotrading.domain.process.repository.ProcessRepository
 import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
-import com.dingdongdeng.autotrading.domain.trade.service.CoinTradeService
 import com.dingdongdeng.autotrading.infra.common.annotation.UseCase
 import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
@@ -18,8 +18,7 @@ class CoinBackTestUseCase(
     private val processRepository: ProcessRepository,
     private val autoTradeProcessorFactory: AutoTradeProcessorFactory,
     private val backTestProcessorFactory: BackTestProcessorFactory,
-    private val coinTradeService: CoinTradeService,
-    private val coinChartService: CoinChartService,
+    private val coinBackTestService: CoinBackTestService,
 ) {
     fun backTest(
         startDateTime: LocalDateTime,
@@ -52,28 +51,8 @@ class CoinBackTestUseCase(
         return backTestProcessor.id
     }
 
-    fun getResult(backTestProcessorId: String) {
+    fun getResult(backTestProcessorId: String): CoinBackTestResult {
         val processor = processRepository.findById(backTestProcessorId) as CoinBackTestProcessor
-
-        val backTestResults = processor.getResults()
-        /**
-         * FIXME 아래 내용을 해보자
-         *  화면을 먼저 기획해보자
-         *  - 내 벡테스트 목록을 조회
-         *  - 백테스트 결과 조회
-         *      - 월별 수익율
-         *          - 코인별 수익율
-         *      - 월별 승율
-         *          - 코인별 승율
-         *      - 주문 리스트 (매수, 익절, 손절)
-         *      - 시물레이션한 차트 리스트
-         *          - 캔들 정보
-         *          - 보조지표 정보
-         *          - 주문정보
-         */
-    }
-
-    fun getSimulation() {
-        // FIXME 차트를 그리기 위한 api...
+        return coinBackTestService.getResults(processor)
     }
 }
