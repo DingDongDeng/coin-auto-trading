6c0fe357985138acbfb0252da921501ecd515c83
DingDongDeng <tpea999@gmail.com>
2024-03-09 01:55:39 +0900

    ss

---
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/model/CoinBackTestResult.kt
@@ -0,0 +1,34 @@
+package com.dingdongdeng.autotrading.domain.backtest.model
+
+import com.dingdongdeng.autotrading.infra.common.type.CoinType
+import com.dingdongdeng.autotrading.infra.common.type.OrderType
+import java.time.LocalDateTime
+
+data class CoinBackTestResult(
+    val backTestProcessorId: String,
+    val progressRate: Double,
+    val startDateTime: LocalDateTime,
+    val endDateTime: LocalDateTime,
+    val profitRate: Double,
+    val profitPrice: Double,
+    val fee: Double,
+    val statistics: Map<CoinType, List<CoinBackTestStatistics>>,
+    val tradeHistories: Map<CoinType, List<CoinBackTestTradeHistory>>,
+)
+
+data class CoinBackTestStatistics(
+    // 기간별 통계
+    val coinType: CoinType,
+    val from: LocalDateTime,
+    val to: LocalDateTime,
+    val profitRate: Double, // 수익율
+    val profitPrice: Double, // 수익금
+)
+
+data class CoinBackTestTradeHistory(
+    val coinType: CoinType,
+    val orderType: OrderType,
+    val volume: Double,
+    val price: Double,
+    val tradeAt: LocalDateTime,
+)
