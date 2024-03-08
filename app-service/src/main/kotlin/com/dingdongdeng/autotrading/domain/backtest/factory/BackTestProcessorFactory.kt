9b15e04a8b0e67ce16adea09bdbf51042dc739a8
DingDongDeng <tpea999@gmail.com>
2024-03-09 01:05:59 +0900

    ss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/factory/BackTestProcessorFactory.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/backtest/factory/BackTestProcessorFactory.kt
@@ -1,10 +1,10 @@
 package com.dingdongdeng.autotrading.domain.backtest.factory
 
+import com.dingdongdeng.autotrading.domain.autotrade.model.CoinAutoTradeProcessor
+import com.dingdongdeng.autotrading.domain.backtest.model.AvailBackTestRange
 import com.dingdongdeng.autotrading.domain.backtest.model.CoinBackTestProcessor
 import com.dingdongdeng.autotrading.domain.chart.service.CoinChartService
-import com.dingdongdeng.autotrading.domain.strategy.component.SpotCoinStrategy
-import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
-import com.dingdongdeng.autotrading.domain.trade.service.CoinTradeService
+import com.dingdongdeng.autotrading.infra.common.exception.WarnException
 import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
@@ -14,33 +14,56 @@ import java.time.LocalDateTime
 @Component
 class BackTestProcessorFactory(
     private val coinChartService: CoinChartService,
-    private val coinTradeService: CoinTradeService,
-    private val strategies: List<SpotCoinStrategy>,
 ) {
 
     fun of(
         startDateTime: LocalDateTime,
         endDateTime: LocalDateTime,
         durationUnit: CandleUnit, // 백테스트 시간 간격
-        userId: Long,
-        coinStrategyType: CoinStrategyType,
-        exchangeType: ExchangeType,
-        coinTypes: List<CoinType>,
-        candleUnits: List<CandleUnit>,
-        config: Map<String, Any>,
+        autoTradeProcessor: CoinAutoTradeProcessor,
     ): CoinBackTestProcessor {
+        autoTradeProcessor.coinTypes.forEach { coinType ->
+            val ranges = getAvailRanges(autoTradeProcessor.exchangeType, coinType, startDateTime, endDateTime)
+            if (ranges.none { it.isRanged(coinType, startDateTime, endDateTime) }) {
+                throw WarnException.of("백테스트 불가능한 구간입니다. availBackTestRanges=$ranges")
+            }
+        }
         return CoinBackTestProcessor(
-            userId = userId,
             startDateTime = startDateTime,
             endDateTime = endDateTime,
             durationUnit = durationUnit,
-            exchangeType = exchangeType,
-            coinTypes = coinTypes,
-            candleUnits = candleUnits,
-            config = config,
-            strategy = strategies.first { it.support(coinStrategyType) },
-            coinChartService = coinChartService,
-            coinTradeService = coinTradeService,
+            autoTradeProcessor = autoTradeProcessor,
         )
+    }
+
+    private fun getAvailRanges(
+        exchangeType: ExchangeType,
+        coinType: CoinType,
+        startDateTime: LocalDateTime,
+        endDateTime: LocalDateTime,
+    ): List<AvailBackTestRange> {
+        val minUnit = CandleUnit.min()
+        val missingDateTimes = coinChartService.getMissingChart(
+            exchangeType = exchangeType,
+            coinType = coinType,
+            candleUnit = minUnit,
+            from = startDateTime,
+            to = endDateTime,
+        ).candles.map { it.candleDateTimeKst }
+
+        val availBackTestRanges = AvailBackTestRange.fromMissingDateTimes(
+            exchangeType = exchangeType,
+            coinType = coinType,
+            candleUnit = minUnit,
+            startDateTime = startDateTime,
+            endDateTime = endDateTime,
+            missingDateTimes = missingDateTimes,
+        )
+
+        return AvailBackTestRange.merge(availBackTestRanges, minUnit.getSecondSize() * ALLOW_MISSING_CANDLE_COUNT)
+    }
+
+    companion object {
+        private const val ALLOW_MISSING_CANDLE_COUNT = 10
     }
 }
