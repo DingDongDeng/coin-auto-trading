c0340e8852bc06a6484aa795630b6d2a44074c0a
DingDongDeng <tpea999@gmail.com>
2024-03-10 23:27:03 +0900

    ss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/model/SpotCoinStrategyMakeTaskParam.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/strategy/model/SpotCoinStrategyMakeTaskParam.kt
@@ -1,7 +1,7 @@
 package com.dingdongdeng.autotrading.domain.strategy.model
 
 import com.dingdongdeng.autotrading.domain.chart.model.Chart
-import com.dingdongdeng.autotrading.domain.trade.model.CoinTradeInfo
+import com.dingdongdeng.autotrading.domain.trade.model.CoinTradeSummary
 import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
 import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
@@ -10,7 +10,7 @@ data class SpotCoinStrategyMakeTaskParam(
     val exchangeType: ExchangeType,
     val coinType: CoinType,
     val charts: List<Chart>, // 1분봉, 5분봉, 60분봉 등
-    val tradeInfo: CoinTradeInfo,
+    val tradeSummary: CoinTradeSummary,
 ) {
     fun getChart(candleUnit: CandleUnit): Chart = charts.first { it.candleUnit == candleUnit }
 }
