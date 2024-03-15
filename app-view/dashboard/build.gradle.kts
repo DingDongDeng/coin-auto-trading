d3c388eb0545d1c538cca7c79f2678d9775851d9
DingDongDeng <tpea999@gmail.com>
2024-03-15 19:34:01 +0900

    front

--- app-view/dashboard/build.gradle.kts
+++ app-view/dashboard/build.gradle.kts
@@ -1,24 +1,3 @@
-import com.github.gradle.node.npm.task.NpmTask
-
 plugins {
     id("project.npm-conventions")
-}
-
-node {
-    version = "21.7.1" // 사용할 Node.js 버전
-    npmVersion = "10.5.0" // 사용할 npm 버전
-    download = true // Node.js 및 npm 자동 다운로드 여부
-}
-
-tasks.register<NpmTask>("npmBuild") {
-    dependsOn("npmInstall")
-    dependsOn("clean")
-
-    args = listOf("run", "build")
-}
-
-tasks.register("npmClean") {
-    doFirst {
-        delete("dist")
-    }
 }
--- buildSrc/src/main/kotlin/project.npm-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/project.npm-conventions.gradle.kts
@@ -1,3 +1,31 @@
+import com.github.gradle.node.npm.task.NpmTask
+
 plugins {
     id("com.github.node-gradle.node")
+}
+
+node {
+    version = "21.7.1" // 사용할 Node.js 버전
+    npmVersion = "10.5.0" // 사용할 npm 버전
+    download = true // Node.js 및 npm 자동 다운로드 여부
+}
+
+tasks.register<NpmTask>("npmBuild") {
+    dependsOn("npmInstall")
+    dependsOn("npmClean")
+
+    args = listOf("run", "build")
+}
+
+tasks.register<NpmTask>("npmRun") {
+    dependsOn("npmInstall")
+    dependsOn("npmClean")
+
+    args = listOf("run", "serve")
+}
+
+tasks.register("npmClean") {
+    doFirst {
+        delete("dist")
+    }
 }
