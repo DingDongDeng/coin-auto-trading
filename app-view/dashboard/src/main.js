da3a474bae3b976bdb313dd38918828d788417c1
lcw3176 <lcw3176@naver.com>
2024-03-16 15:47:55 +0900

    vuetify 기본 설정

--- app-view/dashboard/src/main.js
+++ app-view/dashboard/src/main.js
@@ -1,4 +1,22 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  }
+  
+})
+
+createApp(App).use(vuetify).use(router).mount('#app')
