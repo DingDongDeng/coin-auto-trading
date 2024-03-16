82beb2d0401c1a48ef7f8047000ab2dfc71881c9
lcw3176 <lcw3176@naver.com>
2024-03-16 16:37:57 +0900

    스토어 아웃라인

---
+++ app-view/dashboard/src/store/backtest.js
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+import axios from 'axios';
+
+// async function getContent(id) {
+//   const response = await axios.get('http://localhost:8080/' + id);
+//   return response.data;
+// }
+
+
+// async function getSummary(id) {
+//   const response = await axios.get('http://localhost:8080/' + id);
+//   return response.data;
+// }
+
+
+
+export const useNoticeStore = defineStore("backtestStore", {
+
+  state: () => ({
+    notice: Object,
+    notices: [],
+    lastLoadedId: 0,
+    loading: false,
+  }),
+
+
+  getters: {
+
+  },
+
+  actions: {
+    async loadData(id) {
+
+    }
+  }
+});
