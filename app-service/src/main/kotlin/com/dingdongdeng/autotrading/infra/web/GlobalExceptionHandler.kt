89421a0b07b9944a0a22cde2f2bec24826d03f45
DingDongDeng <tpea999@gmail.com>
2024-03-17 20:33:13 +0900

    작업중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/web/GlobalExceptionHandler.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/web/GlobalExceptionHandler.kt
@@ -6,6 +6,8 @@ import com.dingdongdeng.autotrading.infra.common.exception.WarnException
 import com.dingdongdeng.autotrading.infra.common.log.Slf4j.Companion.log
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindException
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -13,6 +15,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalExceptionHandler(
     private val slackSender: SlackSender,
 ) {
+
+    @ExceptionHandler(BindException::class)
+    fun handleNotValidException(
+        e: BindException,
+        bindingResult: BindingResult
+    ): ResponseEntity<CommonResponse<Unit>> {
+        log.warn("${e.message}, validation errors : ${bindingResult.fieldErrors}", e)
+        return ResponseEntity(
+            CommonResponse(message = bindingResult.fieldError?.defaultMessage),
+            HttpStatus.BAD_REQUEST,
+        )
+    }
 
     @ExceptionHandler(WarnException::class)
     fun handler(e: WarnException): ResponseEntity<CommonResponse<Unit>> {
