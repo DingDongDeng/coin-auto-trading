c68702f7cd93d0bf51ae22dc3fe7bbd9571f8022
DingDongDeng <tpea999@gmail.com>
2024-03-22 00:25:56 +0900

    ss

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/type/ExchangeType.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/infra/common/type/ExchangeType.kt
@@ -2,10 +2,11 @@ package com.dingdongdeng.autotrading.infra.common.type
 
 enum class ExchangeType(
     override val desc: String,
+    val asReal: ExchangeType,
 ) : DescriptionType {
-    UPBIT("업비트"),
-    BINANCE("바이낸스"),
-    BACKTEST_UPBIT("업비트 백테스트")
+    UPBIT("업비트", UPBIT),
+    BINANCE("바이낸스", BINANCE),
+    BACKTEST_UPBIT("업비트 백테스트", UPBIT)
     ;
 
     companion object {
