28a3ca1b97928c1687864ee8345770c7fcaf8abd
DingDongDeng <tpea999@gmail.com>
2024-08-15 22:24:38 +0900

    누락 캔들 로직이 불필요함

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/chart/model/Chart.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/domain/chart/model/Chart.kt
@@ -6,10 +6,11 @@ import java.time.LocalDateTime
 data class Chart(
     val from: LocalDateTime,
     val to: LocalDateTime,
-    val currentPrice: Double,
     val candleUnit: CandleUnit,
     val candles: List<Candle>,
 ) {
+    val currentPrice: Double = if (candles.isEmpty()) 0.0 else candles.last().closingPrice
+
     // 마지막 N번째 요소 (1이 마지막 요소)
     fun getLast(cnt: Int): Candle = candles[candles.size - cnt]
 }
