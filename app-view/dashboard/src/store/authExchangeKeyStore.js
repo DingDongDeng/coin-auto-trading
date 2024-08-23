24170ed296d730267c622fcd8380982795a99a10
DingDongDeng <tpea999@gmail.com>
2024-08-23 21:52:22 +0900

    프론트 작업 중

---
+++ app-view/dashboard/src/store/authExchangeKeyStore.js
@@ -0,0 +1,23 @@
+import {defineStore} from "pinia";
+import axios from 'axios';
+
+async function getExchangeKeys(userId) {
+    const response = await axios.get('http://localhost:8080/coin/exchange-key', {params: {userId}});
+    return response.data;
+}
+
+
+export const useAuthExchangeKeyStore = defineStore("authExchangeKey", {
+
+    state: () => ({
+        exchangeKeys: []
+    }),
+
+    getters: {},
+
+    actions: {
+        async loadData(userId) {
+            this.exchangeKeys = getExchangeKeys(userId).body
+        }
+    }
+});
---
+++ app-view/dashboard/src/views/AuthExchangeKeyView.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import {useAuthExchangeKeyStore} from '@/store/authExchangeKeyStore';
+    import {storeToRefs} from 'pinia'
+    import PanelComponent from "@/components/AuthExchangeKeyDetailPanel.vue";
+
+    const authExchangeKey = useAuthExchangeKeyStore()
+    storeToRefs(authExchangeKey)
+
+    authExchangeKey.loadData('12345')
+</script>
+
+<template>
+    <v-container>
+        <v-row>
+            <v-col v-for="(exchangeKey, i) in authExchangeKey.exchangeKeys" :key="i" cols="auto">
+                <PanelComponent :title="exchangeKey.keyPairId"/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
