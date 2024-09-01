3e3e11bf6dbbaa75453c262760f6738b0ee750fe
DingDongDeng <tpea999@gmail.com>
2024-09-01 14:52:14 +0900

    프론트 작업 중

--- app-service/src/main/kotlin/com/dingdongdeng/autotrading/presentation/dashboard/controller/CodeController.kt
+++ app-service/src/main/kotlin/com/dingdongdeng/autotrading/presentation/dashboard/controller/CodeController.kt
@@ -1,5 +1,8 @@
 package com.dingdongdeng.autotrading.presentation.dashboard.controller
 
+import com.dingdongdeng.autotrading.domain.strategy.type.CoinStrategyType
+import com.dingdongdeng.autotrading.infra.common.type.CandleUnit
+import com.dingdongdeng.autotrading.infra.common.type.CoinType
 import com.dingdongdeng.autotrading.infra.common.type.ExchangeType
 import com.dingdongdeng.autotrading.infra.web.CommonResponse
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +17,27 @@ class CodeController {
     fun getExchangeKeys(): CommonResponse<List<ExchangeType>> {
         return CommonResponse(
             body = ExchangeType.ofNotBackTest()
+        )
+    }
+
+    @GetMapping("/candle-unit")
+    fun getCandleUnits(): CommonResponse<List<CandleUnit>> {
+        return CommonResponse(
+            body = CandleUnit.entries
+        )
+    }
+
+    @GetMapping("/coin-strategy-type")
+    fun getCoinStrategyTypes(): CommonResponse<List<CoinStrategyType>> {
+        return CommonResponse(
+            body = CoinStrategyType.entries
+        )
+    }
+
+    @GetMapping("/coin-type")
+    fun getCoinTypes(): CommonResponse<List<CoinType>> {
+        return CommonResponse(
+            body = CoinType.entries
         )
     }
 }
