17119a2caaddcd5a185329d5f210566ec0e0e017
DingDongDeng <tpea999@gmail.com>
2024-10-05 15:13:48 +0900

    자동매매 만들어보자

--- app-view/dashboard/src/store/autoTradingStore.js
+++ app-view/dashboard/src/store/autoTradingStore.js
@@ -6,17 +6,50 @@ async function getAutoTradings() {
     return response.data;
 }
 
+async function registerBackTest(body) {
+    const response = await axios.post('/coin/processor/autotrade', body);
+    return response.data;
+}
+
+
 export const useAutoTradingStore = defineStore("autoTrading", {
 
     state: () => ({
         autoTradings: [],
+        register: {
+            visibleDialog: false,
+            title: '',
+            coinStrategyType: '',
+            exchangeType: '',
+            coinTypes: [],
+            candleUnits: [],
+            keyPairId: '',
+            config: {},
+            duration: 0,
+        }
     }),
 
     getters: {},
 
     actions: {
-        async loadBackTests() {
+        async loadAutoTradings() {
             this.autoTradings = (await getAutoTradings()).body
         },
+        async registerAutoTrading() {
+            await registerBackTest(this.register)
+            await this.loadAutoTradings()
+            this.registerReset()
+        },
+        registerReset() {
+            this.register.visibleDialog = false;
+            this.register.title = ''
+            this.register.coinStrategyType = ''
+            this.register.exchangeType = ''
+            this.register.coinTypes = []
+            this.register.candleUnits = []
+            this.register.keyPairId = ''
+            this.register.config = {}
+            this.register.duration = 0
+        }
     }
 });
